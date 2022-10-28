d3ae40a9c40ce8ffaa77cb326f059dabe3390828
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-10-28 09:13:27 -0400

    change to run 127

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=RY_mapChIPseq_Run109
+#SBATCH --job-name=RY_mapChIPseq_Run127
 #SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
@@ -17,7 +17,7 @@ cd $SLURM_SUBMIT_DIR
 source config.txt
 
 ##make output directory
-OUTDIR= "/scratch/ry00555/OutputRun109"
+OUTDIR= "/scratch/ry00555/OutputRun127"
 if [ ! -d $OUTDIR ]
 then
 mkdir -p $OUTDIR
