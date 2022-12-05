a6a52c6abe8afb67d0f4755a6a25ac53f77cba04
ry00555 <111528595+ry00555@users.noreply.github.com>
2022-12-05 09:22:19 -0500

    Remake bam files in new dir
    Create a V2 to create brand new bam files aligned to GCF+000182925.2.fasta genome. The genome should already be indexed. Then move files into local directory /Desktop/BamFiles

---
+++ MapCutAndRun109.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --job-name=RY_mapChIPseq_Run127
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ry00555@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=50gb
+#SBATCH --time=48:00:00
+#SBATCH --output=../MapCutAndRun.%j.out
+#SBATCH --error=../MapCutAndRun.%j.err
+
+cd $SLURM_SUBMIT_DIR
+
+#read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
+
+source config.txt
+
+##make output directory
+OUTDIR= "/scratch/ry00555/OutputRun109/V2"
+if [ ! -d $OUTDIR ]
+then
+mkdir -p $OUTDIR
+fi
+
+# #process reads using trimGalore
+#
+ ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+ trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+#
+FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
+#
+mkdir "${OUTDIR}/SortedBamFiles"
+mkdir "${OUTDIR}/BigWigs"
+mkdir "${OUTDIR}/Peaks"
+#mkdir "$OUTDIR/HomerTagDirectories"
+#mkdir "$OUTDIR/TdfFiles"
+#
+#Iterate over the files
+for f in $FILES
+do
+#
+# 	#Examples to Get Different parts of the file name
+# 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
+		#${string//substring/replacement}
+# 		#dir=${f%/*}
+
+	file=${f##*/}
+	#remove ending from file name to create shorter names for bam files and other downstream output
+	name=${file/%_S[1-99]*_R1_001_val_1.fq.gz/}
+
+#
+# 	# File Vars
+# 	#use sed to get the name of the second read matching the input file
+	read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+	#variable for naming bam file
+ 	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+	#variable name for bigwig output
+	bigwig="${OUTDIR}/BigWigs/${name}"
+	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+#
+
+ml SAMtools/1.9-GCC-8.3.0
+ml BWA/0.7.17-GCC-8.3.0
+#sampe -r STR adds a read group to each sample.
+bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+samtools index "$bam"
+
+#samtools view -b -q 30 $bam > "$QualityBam"
+#samtools index "$QualityBam"
+
+############################
+# # #deeptools
+
+ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+#Plot all reads
+bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+
+#plot mononucleosomes
+#bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
+
+done
