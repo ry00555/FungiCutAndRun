44209e77233bcae079d40ee0635c200fa970a0d9
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-06-28 13:40:51 -0400

    Update MapCutAndRun.sh
    try removing the bam index as its alreafdy done

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -63,8 +63,8 @@ do
 ml SAMtools/1.9-GCC-8.3.0
 ml BWA/0.7.17-GCC-8.3.0
 #sampe -r STR adds a read group to each sample.
-bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-samtools index "$bam"
+#bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+#samtools index "$bam"
 
 #samtools view -b -q 30 $bam > "$QualityBam"
 #samtools index "$QualityBam"
