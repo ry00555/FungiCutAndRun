f2142babc7b5ca9ee01d5a79b61afd2ba49078db
Rochelle Yap <ry00555@ss-sub3.gacrc.uga.edu>
2023-07-23 19:15:59 -0400

    hi

--- ChIPseqPipeline.sh
+++ ChIPseqPipeline.sh
@@ -18,23 +18,18 @@ source config.txt
 
 OUTDIR=/scratch/ry00555/OutputRun132
 
-
 # Process reads using trimGalore
 #ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 
-#ml SAMtools/1.9-GCC-8.3.0
+ml SAMtools/1.9-GCC-8.3.0
 
 ml BWA/0.7.17-GCC-8.3.0
 
-module load Homer/4.11-foss-2019b SAMtools/1.16.1-GCC-11.3.0
-
-
-
+module load Homer/4.11-foss-2019b
 
 #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 
 FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
-
 
 #mkdir "${OUTDIR}/SortedBamFiles"
 #mkdir "${OUTDIR}/BigWigs"
@@ -52,12 +47,9 @@ FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
 #  bam="${OUTDIR}/SortedBamFiles/${name}.bam"
   bigwig="${OUTDIR}/BigWigs/${name}"
 
-
-
   #bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
   #samtools index "$bam"
-
-#  ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+ ml deepTools/3.3.1-intel-2019b-Python-3.7.4
 #  conda install -c bioconda deeptools
 
 #  bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
@@ -80,8 +72,8 @@ for bam_file in "${BAMDIR}"/*.bam; do
   makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
 
   # Call peaks
-  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt" -i "${BAMDIR}/${sample_id}.bam"
-
+#  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt" -i "${BAMDIR}/${sample_id}.bam"
+  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}/${sample_id}_peaks.txt" -i "${BAMDIR}/${bam_file}.bam"
   # Normalize to mitochondrial DNA which has no nucleosomes
   # Calculate read counts using samtools idxstats
    #mt_read_count=$(samtools idxstats "${bam_file}" | awk '$1=="MT"{print $3}')
@@ -93,12 +85,12 @@ for bam_file in "${BAMDIR}"/*.bam; do
    # Normalize the ChIP-seq signal using bamCoverage with the scaling factor
    #bamCoverage --scaleFactor $scaling_factor -of bigwig -b "${bam_file}".bam -o "${OUTDIR}/NormalizedBigWigs/${sample_id}_normalized.bw"
    #bamCoverage -of bigwig -b "${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
-   bamCoverage -b "${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
+   bamCoverage -b "${PEAKDIR}/${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
  done
 
 
-module unload Homer/4.11-foss-2019b SAMtools/1.16.1-GCC-11.3.0
-ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+module unload Homer/4.11-foss-2019b
+#ml deepTools/3.3.1-intel-2019b-Python-3.7.4
 
 # Iterate over each file in the directory
 for file_path in "${OUTDIR}/BigWigs"/*.bw; do
