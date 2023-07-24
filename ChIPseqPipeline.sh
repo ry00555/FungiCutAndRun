c73479d8abac9e57143ef2a5e8a895b67bb48220
ry00555 <ry00555@uga.edu>
2023-07-24 12:47:04 -0400

    ShanHo Suggestion
    removing -i in find peaks

--- ChIPseqPipeline.sh
+++ ChIPseqPipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Run132ChIP
+#SBATCH --job-name=ChIPSeqPipeline
 #SBATCH --partition=batch
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
@@ -7,29 +7,27 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=50gb
 #SBATCH --time=48:00:00
-#SBATCH --output=../MapCutAndRun132.%j.out
-#SBATCH --error=../MapCutAndRun132.%j.err
+#SBATCH --output=../ChIPSeqPipeline132.%j.out
+#SBATCH --error=../ChIPSeqPipeline132.%j.err
 
 cd $SLURM_SUBMIT_DIR
 
 #read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
 
-source config.txt
+#source config.txt
 
 OUTDIR=/scratch/ry00555/OutputRun132
+ml SAMtools/1.16.1-GCC-10.2.0
+ml BWA/0.7.17-GCC-10.2.0
+ml Homer/4.11-foss-2020b
+ml deepTools/3.5.1-foss-2020b-Python-3.8.6
 
 # Process reads using trimGalore
 #ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
-
-ml SAMtools/1.9-GCC-8.3.0
-
-ml BWA/0.7.17-GCC-8.3.0
-
-module load Homer/4.11-foss-2019b
-
 #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 
 FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
+
 
 #mkdir "${OUTDIR}/SortedBamFiles"
 #mkdir "${OUTDIR}/BigWigs"
@@ -47,21 +45,23 @@ FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
 #  bam="${OUTDIR}/SortedBamFiles/${name}.bam"
   bigwig="${OUTDIR}/BigWigs/${name}"
 
+
+
   #bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
   #samtools index "$bam"
- ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+
+#  ml deepTools/3.3.1-intel-2019b-Python-3.7.4
 #  conda install -c bioconda deeptools
 
 #  bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 #  bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
-#done D 
+#done
 
 # Set common variables
 PEAKDIR="${OUTDIR}/Peaks"
 TAGDIR="${OUTDIR}/HomerTagDirectories"
 BAMDIR="${OUTDIR}/SortedBamFiles"
 
-# Iterate over each BAM file in the directory #hi
 # Iterate over each BAM file in the directory
 for bam_file in "${BAMDIR}"/*.bam; do
   # Get the sample ID from the BAM file name
@@ -73,37 +73,22 @@ for bam_file in "${BAMDIR}"/*.bam; do
   makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
 
   # Call peaks
-<<<<<<< HEAD
-#  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt" -i "${BAMDIR}/${sample_id}.bam"
-  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}/${sample_id}_peaks.txt" -i "${BAMDIR}/${bam_file}.bam"
-=======
-  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}/${sample_id}_peaks.txt" -i "${bam_file}"
+  findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt"
 
->>>>>>> 8c86216ba6c982cfdaca6000e1e84bd4e4888317
   # Normalize to mitochondrial DNA which has no nucleosomes
   # Calculate read counts using samtools idxstats
-  mt_read_count=$(samtools idxstats "${bam_file}" | awk '$1=="MT"{print $3}')
-  reference_read_count=$(samtools idxstats "${bam_file}" | awk 'BEGIN{total=0}{if($1!="MT"){total+=$3}}END{print total}')
+   #mt_read_count=$(samtools idxstats "${bam_file}" | awk '$1=="MT"{print $3}')
+   #reference_read_count=$(samtools idxstats "${bam_file}" | awk 'BEGIN{total=0}{if($1!="MT"){total+=$3}}END{print total}')
 
-<<<<<<< HEAD
    # Calculate scaling factor
    #scaling_factor=$(bc <<< "scale=4; ${mt_read_count} / ${reference_read_count}")
 
    # Normalize the ChIP-seq signal using bamCoverage with the scaling factor
    #bamCoverage --scaleFactor $scaling_factor -of bigwig -b "${bam_file}".bam -o "${OUTDIR}/NormalizedBigWigs/${sample_id}_normalized.bw"
    #bamCoverage -of bigwig -b "${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
-   bamCoverage -b "${PEAKDIR}/${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
+   bamCoverage -b "${bam_file}" -o "${OUTDIR}/BigWigs/${sample_id}.bw"
  done
-=======
-  # Calculate scaling factor
-  scaling_factor=$(bc <<< "scale=4; ${mt_read_count} / ${reference_read_count}")
-  bamCoverage --scaleFactor $scaling_factor -of bigwig -b "${bam_file}" -o "/scratch/ry00555/OutputRun132/NormalizedBigWigs/${sample_id}_normalized.bw"
-done
->>>>>>> 8c86216ba6c982cfdaca6000e1e84bd4e4888317
 
-
-module unload Homer/4.11-foss-2019b
-#ml deepTools/3.3.1-intel-2019b-Python-3.7.4
 
 # Iterate over each file in the directory
 for file_path in "${OUTDIR}/BigWigs"/*.bw; do
