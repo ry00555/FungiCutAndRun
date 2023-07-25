09c1089b294f9a1db9fa6bd341ac451defac3bcb
ry00555 <ry00555@uga.edu>
2023-07-25 14:35:56 -0400

    Update ChIPseqPipeline.sh
    still stuck on normalization

--- ChIPseqPipeline.sh
+++ ChIPseqPipeline.sh
@@ -20,11 +20,11 @@ source config.txt
 OUTDIR=/scratch/ry00555/OutputRun132
 
 #Load all the modules that are needed for the entire pipeline
-ml SAMtools/1.16.1-GCC-10.2.0
-ml BWA/0.7.17-GCC-10.2.0
+#ml SAMtools/1.16.1-GCC-10.2.0
+#ml BWA/0.7.17-GCC-10.2.0
 ml Homer/4.11-foss-2020b
 ml deepTools/3.5.1-foss-2020b-Python-3.8.6
-ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+#ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 
 #This can be changed but make sure that there are appropriate paths and output directories based on the analysis specifically when peak calling and normalizing
 
@@ -39,25 +39,25 @@ ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 
 # Process reads using trimGalore
 
-trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+#trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 
-FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
+#FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz"
 
 # Iterate over the files
-for f in $FILES
-do
- file=${f##*/}
-  name=${file/%_S[1-99]*_R1_001_val_1.fq.gz/}
+#for f in $FILES
+#do
+ #file=${f##*/}
+  #name=${file/%_S[1-99]*_R1_001_val_1.fq.gz/}
 
-  read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
-  bam="${OUTDIR}/SortedBamFiles/${name}.bam"
-  bigwig="${OUTDIR}/BigWigs/${name}"
+  #read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+  #bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+  #bigwig="${OUTDIR}/BigWigs/${name}"
 
-  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-  samtools index "$bam"
-  bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+  #bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+  #samtools index "$bam"
+  #bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 #  bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
-done
+#done
 
 # Set common variables - NOTE this is my organization method, do what works best for you, but if you change these variables then you might need to change the paths and variables below
 PEAKDIR="${OUTDIR}/Peaks"
@@ -72,10 +72,10 @@ for bam_file in "${BAMDIR}"/*.bam; do
   sample_id="${sample_id%%_Rep_1*}"
 
   # Make tag directory
- makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
+ #makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
 
   # Call peaks
- findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt"
+ #findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${TAGDIR}/${sample_id}_peaks.txt"
 
  # Calculate read counts using samtools idxstats
   mt_read_count=$(samtools idxstats "${bam_file}" | awk '$1=="MT"{print $3}')
