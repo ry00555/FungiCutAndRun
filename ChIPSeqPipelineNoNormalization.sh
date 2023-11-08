519afd46d7b9d2009bee6df3dfbbaf27dd09fdc6
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-11-08 17:10:36 -0500

    Update ChIPSeqPipelineNoNormalization.sh
    try without peak calling

--- ChIPSeqPipelineNoNormalization.sh
+++ ChIPSeqPipelineNoNormalization.sh
@@ -24,8 +24,8 @@ ml BEDTools/2.30.0-GCC-11.3.0
 
 #mkdir -p "${OUTDIR}/SortedBamFiles"
 #mkdir -p "${OUTDIR}/BigWigs"
-mkdir -p "${OUTDIR}/Peaks"
-mkdir -p "$OUTDIR/HomerTagDirectories"
+#mkdir -p "${OUTDIR}/Peaks"
+#mkdir -p "$OUTDIR/HomerTagDirectories"
 #mkdir -p "$OUTDIR/TdfFiles"
 #mkdir -p "${OUTDIR}/Heatmaps"
 #mkdir -p "${OUTDIR}/Matrices"
@@ -34,22 +34,22 @@ mkdir -p "$OUTDIR/HomerTagDirectories"
 #mkdir -p "${OUTDIR}/Counts"
 
 
-PEAKDIR="${OUTDIR}/Peaks"
-TAGDIR="${OUTDIR}/HomerTagDirectories"
-BAMDIR="${OUTDIR}/SortedBamFiles"
-BEDDIR="${OUTDIR}/Beds"
+#PEAKDIR="${OUTDIR}/Peaks"
+#TAGDIR="${OUTDIR}/HomerTagDirectories"
+#BAMDIR="${OUTDIR}/SortedBamFiles"
+#BEDDIR="${OUTDIR}/Beds"
 
-for bam_file in "${BAMDIR}"/*.bam; do
+#for bam_file in "${BAMDIR}"/*.bam; do
   # Get the sample ID from the BAM file name
-  sample_id=$(basename "${bam_file}" .bam)
+#  sample_id=$(basename "${bam_file}" .bam)
   # Remove everything after "Rep_1" in the sample ID
-  sample_id="${sample_id%%_Rep_1*}"
+#  sample_id="${sample_id%%_Rep_1*}"
 # Check if the sample_id contains "input" and set the -i argument accordingly
- if [[ "${sample_id}" == *input* ]]; then
+# if [[ "${sample_id}" == *input* ]]; then
    findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}_peaks.txt" -i "${TAGDIR}/${sample_id}"
- else
-   findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}_peaks.txt"
- fi
+# else
+#   findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}_peaks.txt"
+# fi
 
  # for file_path in "${OUTDIR}/BigWigs"/*.bw; do
  #   # Get the base name of the file
@@ -63,7 +63,7 @@ for bam_file in "${BAMDIR}"/*.bam; do
  #  # Limit the length of the sample ID to avoid long filenames
  #  BW_id=${BW_id:0:50}
   # Compute matrix for the reference-point TSS
-done
+#done
   computeMatrix reference-point --referencePoint TSS -b 1500 -a 1500 -S 133-90_ChIP_WT_H3K27me3_Rep1_S87_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw 133-78_ChIP_NCU00423_H3K27me3_Rep1_S75_L001_R1_001_val_1.fq.gz.bin_25.smooth_75Bulk.bw -R "/scratch/ry00555/neurospora.bed" --skipZeros -o "${OUTDIR}/Matrices/NCU00423_H3K27me3.gz"
   plotHeatmap --matrixFile "${OUTDIR}/Matrices/NCU00423_H3K27me3.gz" --outFileName "${OUTDIR}/Heatmaps/NCU00423_H3K27me3_hclust.png" --samplesLabel WT NCU00423-KO --hclust 1 --colorMap Reds --sortRegions descend --missingDataColor white --sortUsingSamples 1
 
