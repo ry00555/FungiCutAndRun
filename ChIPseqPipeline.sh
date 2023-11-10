d2d9ade1823477686abdaf32f8eea07d3b8e870c
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-11-10 15:10:44 -0500

    Updating entire directory

--- ChIPseqPipeline.sh
+++ ChIPseqPipeline.sh
@@ -184,7 +184,7 @@ for file_path in "${OUTDIR}/NormalizedBigWigs"/*.bw; do
   zcat "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz" | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}_processed.gz"
 
   # Plot heatmaps for the reference-point TSS
- plotHeatmap --matrixFile "${OUTDIR}/Matrices/matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Reds
+ plotHeatmap --matrixFile "${OUTDIR}/Matrices/matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Reds --sortRegions descend --missingDataColor white --sortUsingSamples 1
 
   # Plot heatmaps for the reference-point TSS with specific regions (e.g., PRC2 genes)
  plotHeatmap --matrixFile "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust_PRC2genes.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Reds
