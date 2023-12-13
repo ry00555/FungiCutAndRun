6be158419c6e5e91e56c35e85af2b5b6ce7a17a8
ry00555 <111528595+ry00555@users.noreply.github.com>
2023-12-13 13:04:00 -0500

    Index p edwinni file already
    Create a script to analyze CHIP for Kurt's lab
    
    bwa index GCA_019428685.1_ASM1942868v1_genomic.fna

---
+++ ChIPforDahlstrom.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --job-name=DahlstrohmChIP
+#SBATCH --partition=batch
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ry00555@uga.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=50gb
+#SBATCH --time=48:00:00
+#SBATCH --output=../DahlstrohmChIP.%j.out
+#SBATCH --error=../DahlstrohmChIP.%j.err
+
+cd $SLURM_SUBMIT_DIR
+
+#read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
+
+source ConfigforDahlstrom.txt
+
+OUTDIR=/scratch/ry00555/DahlstromRun135/Output
+if [ ! -d $OUTDIR ]
+then
+mkdir -p $OUTDIR
+fi
+
+
+# #process reads using trimGalore
+#
+ ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
+ trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+#
+FILES="/scratch/ry00555/DahlstromRun135/*R1_001_val_1\.fq\.gz" #Don't forget the *
+#
+mkdir "${OUTDIR}/SortedBamFiles"
+mkdir "${OUTDIR}/BigWigs"
+mkdir "${OUTDIR}/Peaks"
+mkdir "$OUTDIR/HomerTagDirectories"
+mkdir "$OUTDIR/TdfFiles"
+#
+#Iterate over the files
+for f in $FILES
+do
+#
+# 	#Examples to Get Different parts of the file name
+# 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
+		#${string//substring/replacement}
+# 		#dir=${f%/*}
+
+	file=${f##*/}
+	#remove ending from file name to create shorter names for bam files and other downstream output
+	name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
+
+#
+# 	# File Vars
+# 	#use sed to get the name of the second read matching the input file
+	read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+	#variable for naming bam file
+ 	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+	#variable name for bigwig output
+	bigwig="${OUTDIR}/BigWigs/${name}"
+	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+#
+
+ml SAMtools/1.9-GCC-8.3.0
+ml BWA/0.7.17-GCC-8.3.0
+#
+bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+samtools index "$bam"
+
+#samtools view -b -q 30 $bam > "$QualityBam"
+#samtools index "$QualityBam"
+
+############################
+# # #deeptools
+
+ml deepTools/3.3.1-intel-2019b-Python-3.7.4
+#Plot all reads
+bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+
+#plot mononucleosomes
+bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
+
+done
