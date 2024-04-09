bd66b0edf298977e5c0b25809c0b546f1a5c77ae
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-09 10:19:13 -0400

    Update CnRDNAKmet.sh
    permission to make directories still denied. Just make manually and toggle out those commands. Then change FASTQ. variable to only OUTDIR

--- CnRDNAKmet.sh
+++ CnRDNAKmet.sh
@@ -11,16 +11,16 @@
 #SBATCH --error=../MapCutAndRun132.%j.err
 
 OUTDIR= "/scratch/ry00555/OutputRun137/CutandRun"
-FASTQ= "/scratch/ry00555/OutputRun137/CutandRun"
+#FASTQ= "/scratch/ry00555/OutputRun137/CutandRun"
 
 ml Trim_Galore
 #starting with raw files in $OUTDIR/raw
-mkdir "$OUTDIR/TrimmedReads"
-trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+#mkdir "$OUTDIR/TrimmedReads"
+trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${OUTDIR}/*fastq\.gz
 
 FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
 
-mkdir "$OUTDIR/ref"
+#mkdir "$OUTDIR/ref"
 curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > $OUTDIR/ref/ecoli_refseq.fa
 curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/182/925/GCF_000182925.2_NC12/GCF_000182925.2_NC12_genomic.fna.gz | gunzip -c > $OUTDIR/ref/Ncrassa_refseq.fa
 
@@ -41,11 +41,11 @@ bowtie2-build -f $OUTDIR/ref/ecoli_refseq.fa $OUTDIR/ref/ecoli_ref
 bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x $OUTDIR/ref/ecoli_ref -1 $f -2 $read2 -S $OUTDIR/sam_files/${name}_Ecoli.sam
 
 module load SAMtools
-mkdir "$OUTDIR/bam_files"
+#mkdir "$OUTDIR/bam_files"
 samtools view -bS -h $OUTDIR/sam_files/${name}.sam  > $OUTDIR/bam_files/${name}.bam
 samtools view -bS -h $OUTDIR/sam_files/${name}_Ecoli.sam  > $OUTDIR/bam_files/${name}_Ecoli.bam
 
-mkdir "$OUTDIR/SortedBamFiles"
+#mkdir "$OUTDIR/SortedBamFiles"
 samtools sort $OUTDIR/bam_files/${name}.bam -o $OUTDIR/SortedBamFiles/${name}.sorted.bam
 samtools sort $OUTDIR/bam_files/${name}_Ecoli.bam -o $OUTDIR/SortedBamFiles/${name}_Ecoli.sorted.bam
 
