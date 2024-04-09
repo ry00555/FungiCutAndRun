39ae68ee32f4f6334d179e110714c7f3ff2cac1d
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-09 13:35:09 -0400

    Update CnRDNAKmet.sh
    time to normalize into bedgraphs

--- CnRDNAKmet.sh
+++ CnRDNAKmet.sh
@@ -33,33 +33,55 @@ module load Bowtie2
 # #in line commands
 # bowtie2-build -f ref/Ncrassa_refseq.fa ref/Ncrassa_ref
 # bowtie2-build -f ref/ecoli_refseq.fa ref/Ecoli_ref
+
+#Use STAR to create chrNameLength.txt files
+# ml STAR
+# STAR --runThreadN 20 --genomeSAindexNbases 8 --runMode genomeGenerate --genomeDir $OUTDIR/ref/Ncrassa_ref --genomeFastaFiles $OUTDIR/ref/Ncrassa_refseq.fa
+# #command line
+# STAR --runThreadN 20 --genomeSAindexNbases 8 --runMode genomeGenerate --genomeFastaFiles Ncrassa_refseq.fa
+
+# for f in $FILES
+#  do
+# file=${f##*/}
+# # 	#remove ending from file name to create shorter names for bam files and other downstream output
+# name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
+# #
+# # #
+# # # 	# File Vars
+# # # 	#use sed to get the name of the second read matching the input file
+# read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+# #mkdir $OUTDIR/sam_files
+# bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ncrassa_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"
 #
+# #bowtie2-build -f ${OUTDIR}/ref/ecoli_refseq.fa $OUTDIR/ref/ecoli_ref
+# bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ecoli_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"
+#
+# module load SAMtools
+# #mkdir "$OUTDIR/bam_files"
+# samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam"
+# samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam"
+#
+# #mkdir "$OUTDIR/SortedBamFiles"
+# samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam"
+# samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam"
+#
+# done
+#no need to samtools merge at the moment because I only have one of each sample
+##turning sorted bam files into bed graphs for DNA spike in
+#mkdir $OUTDIR/bed_files
 for f in $FILES
  do
 file=${f##*/}
 # 	#remove ending from file name to create shorter names for bam files and other downstream output
 name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
-#
-# #
-# # 	# File Vars
-# # 	#use sed to get the name of the second read matching the input file
-read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
-#mkdir $OUTDIR/sam_files
-bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ncrassa_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"
 
-#bowtie2-build -f ${OUTDIR}/ref/ecoli_refseq.fa $OUTDIR/ref/ecoli_ref
-bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ecoli_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"
+ml BEDTools
+bedtools bamtobed -i $OUTDIR/bam_files/${name}.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > $OUTDIR/bed_files/${name}.btb.bed
+bedtools bamtobed -i $OUTDIR/bam_files/${name}_Ecoli.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > $OUTDIR/bed_files/${name}_Ecoli.btb.bed
 
-module load SAMtools
-#mkdir "$OUTDIR/bam_files"
-samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam"
-samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam"
-
-#mkdir "$OUTDIR/SortedBamFiles"
-samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam"
-samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam"
-
+#DNA-spike in normalization
+#mkdir $OUTDIR/bedgraphs
+sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/DNAspike_in.kd.sh $OUTDIR/bed_files/${name}.bed $OUTDIR/bed_files/${name}_Ecoli.btb.bed 100000 bga "/scratch/ry00555/OutputRun137/CutandRun/ref/GenomeDir/chrNameLength.txt" 1 1000 $OUTDIR/bedgraphs/${name}.norm.bga
 done
-#ml SAMtools
-# samtools merge $OUTDIR/bam_files/timecourse_IgG_danio_merged.bam $OUTDIR/bam_files/2.5hpf_IgG.sorted.bam $OUTDIR/bam_files/4.5hpf_IgG.sorted.bam $OUTDIR/bam_files/24hpf_IgG.sorted.bam
-#samtools merge
+#sort bga files from  DNA spike in
+ #ml ucsc
