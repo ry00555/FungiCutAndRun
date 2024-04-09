81e4c6c7c4d01cc34391e6fb7ba402326a3472b9
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-09 16:54:48 -0400

    Update CnRDNAKmet.sh
    nano    Reminder, this recently changed: put actualy genome size, not 2x like before...
    !!! Could not open tag information file: /scratch/ry00555/OutputRun137/CutandRun/TagDirectories/137-*-H3K27me3_Rep1.BtB.tagdir/tagInfo/tagInfo.txt
    Probably not a valid tag directory.  Quitting...
            Reminder, this recently changed: put actualy genome size, not 2x like before...
    !!! Could not open tag information file: /scratch/ry00555/OutputRun137/CutandRun/TagDirectories/137-*-H3K27me3_Rep1.BtB.tagdir/tagInfo/tagInfo.txt
    Probably not a valid tag directory.  Quitting...
    sed: can't read /scratch/ry00555/OutputRun137/CutandRun/TagDirectories/*.txt: No such file or directory

--- CnRDNAKmet.sh
+++ CnRDNAKmet.sh
@@ -110,7 +110,7 @@ module load Homer
 OUTDIR="/scratch/ry00555/OutputRun137/CutandRun/TagDirectories"
 
 # Iterate over the directories
-for infile in $OUTDIR/137-*-H3K27me3_Rep1.BtB.tagdir
+for infile in $OUTDIR/*.tagdir
 do
     # Extract the base name of the directory
     base=$(basename ${infile})
