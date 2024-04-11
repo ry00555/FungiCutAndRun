c68382ca5519ae3054e36bfe5ebe3d2cbf1f969a
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-11 13:11:35 -0400

    Update CnRDNAKmet.sh
    try from the beginning so all bem and sam files are listed the same

--- CnRDNAKmet.sh
+++ CnRDNAKmet.sh
@@ -10,7 +10,7 @@
 #SBATCH --output=../MapCutAndRun132.%j.out
 #SBATCH --error=../MapCutAndRun132.%j.err
 #cd $SLURM_SUBMIT_DIR
-#$OUTDIR="/scratch/ry00555/OutputRun137/CutandRun"
+OUTDIR="/scratch/ry00555/OutputRun137/CutandRun"
 #$FASTQ="/scratch/ry00555/OutputRun137/CutandRun"
 
 # ml Trim_Galore
@@ -40,49 +40,49 @@ FILES="/scratch/ry00555/OutputRun137/CutandRun/TrimmedReads/*R1_001_val_1\.fq\.g
 # #command line
 # STAR --runThreadN 20 --genomeSAindexNbases 8 --runMode genomeGenerate --genomeFastaFiles Ncrassa_refseq.fa
 
-#for f in $FILES
-  #do
- #file=${f##*/}
+for f in $FILES
+  do
+ file=${f##*/}
 # # 	#remove ending from file name to create shorter names for bam files and other downstream output
- #name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
+ name=${file/*_R1_001_val_1.fq.gz/}
 # #
 # # #
 # # # 	# File Vars
 # # # 	#use sed to get the name of the second read matching the input file
- #read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+ read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
 # #mkdir $OUTDIR/sam_files
-# bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ncrassa_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"
+bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ncrassa_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"
 #
 # #bowtie2-build -f ${OUTDIR}/ref/ecoli_refseq.fa $OUTDIR/ref/ecoli_ref
-# bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ecoli_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"
+ bowtie2 --local --very-sensitive-local --phred33 --no-unal -p 24 -x "/scratch/ry00555/OutputRun137/CutandRun/ref/Ecoli_ref" -1 $f -2 $read2 -S "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"
 #
-# module load SAMtools
+module load SAMtools
 # #mkdir "$OUTDIR/bam_files"
-# samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam"
-# samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam"
+samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam"
+samtools view -bS -h "/scratch/ry00555/OutputRun137/CutandRun/sam_files/${name}_Ecoli.sam"  > "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam"
 #
 # #mkdir "$OUTDIR/SortedBamFiles"
-# samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam"
-# samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam"
+samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam"
+samtools sort "/scratch/ry00555/OutputRun137/CutandRun/bam_files/${name}_Ecoli.bam" -o "/scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam"
 #
-# done
+done
 #no need to samtools merge at the moment because I only have one of each sample
 ##turning sorted bam files into bed graphs for DNA spike in
 #mkdir $OUTDIR/bed_files
-# for f in $FILES
-#  do
-# file=${f##*/}
+#for f in $FILES
+  #do
+ #file=${f##*/}
 # # 	#remove ending from file name to create shorter names for bam files and other downstream output
-# name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
+ #name=${file/*_R1_001_val_1.fq.gz/}
 #
-# ml BEDTools
-# bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed
-# bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed
+ #ml BEDTools
+#bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed
+#bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed
 #
 # #DNA-spike in normalization
 # #mkdir $OUTDIR/bedgraphs
 #sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/DNAspike_in.kd.sh /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.bed /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed 100000 bga "/scratch/ry00555/OutputRun137/CutandRun/ref/GenomeDir/chrNameLength.txt" 1 1000 /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/${name}.norm.bga
-# done
+#done
 #sort bga files from  DNA spike in
  # ml ucsc
  # for infile in /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/*norm.bga
@@ -107,7 +107,6 @@ FILES="/scratch/ry00555/OutputRun137/CutandRun/TrimmedReads/*R1_001_val_1\.fq\.g
 #!/bin/bash
 
 # Set the directory containing the tag directories
-OUTDIR="/scratch/ry00555/OutputRun137/CutandRun"
 # # this code works where I have to type in strain  so do it for the rest change rtt109 to WT , set-7, ncu00423, ncu006787, ncu06788
 #
 # ##using IgG as input
@@ -182,9 +181,17 @@ OUTDIR="/scratch/ry00555/OutputRun137/CutandRun"
 #
 
 #kmet spike in for both reg sorted and ecoli sorted
+#The issue is the base name for all the files do not match
+for file in $OUTDIR/SortedBamFiles/*_Ecoli.sorted.bam
+ do
+    base=$(basename "${file}" _Ecoli.sorted.bam)
+ sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/kmet_spike.sh $OUTDIR/KmetSpikeIn/Ecolisortedbedgraphs $base $OUTDIR/TrimmedReads/${base}*R1_001_val_1.fq.gz \
+$OUTDIR/TrimmedReads/${base}*R2_001_val_2.fq.gz $file bga $OUTDIR/ref/GenomeDir/chrNameLength.txt
+done
+
 for file in $OUTDIR/SortedBamFiles/*sorted.bam
  do
-    base=$(basename "${file}" .sorted.bam)
+    base=$(basename "${file}" sorted.bam)
  sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/kmet_spike.sh $OUTDIR/KmetSpikeIn/bedgraphs $base $OUTDIR/TrimmedReads/${base}*R1_001_val_1.fq.gz \
 $OUTDIR/TrimmedReads/${base}*R2_001_val_2.fq.gz $file bga $OUTDIR/ref/GenomeDir/chrNameLength.txt
 done
