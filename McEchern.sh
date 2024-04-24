68b160129b3366cc08bf8a46514dd0c0799f4bb3
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-24 11:34:44 -0400

    Update McEchern.sh

--- McEchern.sh
+++ McEchern.sh
@@ -191,12 +191,11 @@ SORTED_BAM_DIR="/scratch/ry00555/McEachern/SortedBamFiles"
 # # #   # Get the base name of the BAM file
      base_name=$(basename "$bam_file" _output.bam)
 # # #   # Define the output file path
-     input_file="${SORTED_BAM_DIR}/${base_name}"
 #ml SAMtools
 #  samtools index "$SORTED_BAM_DIR/113*_output.bam"
 # # #
   gatk CollectReadCounts \
-   -I "$input_file" \
+   -I "$SORTED_BAM_DIR/113*_output.bam" \
    -R /scratch/ry00555/McEachern/Genome/GCF_000002515.2_ASM251v1_genomic.fna \
    -L /scratch/ry00555/McEachern/Genome/klactis_preprocessed1000_intervals.interval_list \
    --interval-merging-rule OVERLAPPING_ONLY \
