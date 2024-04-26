caa764d201df459d85092026e7cdd5347c579fe2
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-26 11:09:17 -0400

    Update McEchern.sh

--- McEchern.sh
+++ McEchern.sh
@@ -330,10 +330,18 @@ do
 gatk CollectReadCounts \
 -I $bam_file \
 -R /scratch/ry00555/McEachern/Genome/Kluyveromycesmarxianus.fna \
-  -L /scratch/ry00555/McEachern/Genome/ Kluyveromycesmarxianus_preprocessed1000_intervals.interval_list \
+  -L /scratch/ry00555/McEachern/Genome/Kluyveromycesmarxianus_preprocessed1000_intervals.interval_list \
    --interval-merging-rule OVERLAPPING_ONLY \
   -O /scratch/ry00555/McEachern/KmCountTSVs/$base_name.counts.tsv
  done
+
+ gatk CollectReadCounts \
+ -I *_output.bam \
+ -R /scratch/ry00555/McEachern/Genome/Kluyveromycesmarxianus.fna \
+   -L /scratch/ry00555/McEachern/Genome/ Kluyveromycesmarxianus_preprocessed1000_intervals.interval_list \
+    --interval-merging-rule OVERLAPPING_ONLY \
+   -O /scratch/ry00555/McEachern/KmCountTSVs/*.counts.tsv
+  done
 #
 #  KmCounts="/scratch/ry00555/McEachern/KmCountTSVs"
 #  for count_files in $KmCounts/*.counts.tsv
