c93f101399614b0d15ad764f01a98a4b1c457b5b
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-04-26 14:10:28 -0400

    Update McEchern.sh

--- McEchern.sh
+++ McEchern.sh
@@ -423,8 +423,8 @@ for copy_ratios in ${KmAlellicCounts}/*.allelicCounts.tsv
 #     -O ${OUTDIR}/ModelSegments
 #   #
      gatk PlotModeledSegments \
-     --allelic-counts "${copy_ratios}" \ 
- --segments ${OUTDIR}/ModelSegments/${base_name}.modelFinal.seg \
+     --allelic-counts "${copy_ratios}" \
+     --segments ${OUTDIR}/ModelSegments/${base_name}.modelFinal.seg \
      --sequence-dictionary /scratch/ry00555/McEachern/Genome/Kluyveromycesmarxianus.dict \
       --point-size-copy-ratio 1 \
       --minimum-total-allele-count-case 0 \
