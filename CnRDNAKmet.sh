74187ab07a06f7d157a829cda47aa4d927822788
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-05-13 12:03:05 -0400

    Update CnRDNAKmet.sh

--- CnRDNAKmet.sh
+++ CnRDNAKmet.sh
@@ -68,39 +68,39 @@ FILES="/scratch/ry00555/OutputRun137/CutandRun/TrimmedReads/*_R1_001_val_1.fq.gz
 # done
 #no need to samtools merge at the moment because I only have one of each sample
 ##turning sorted bam files into bed graphs for DNA spike in
-#mkdir $OUTDIR/bed_files
-for f in $FILES
-do
-name=$(basename "${f}" _R1_001_val_1.fq.gz)
-#
-ml BEDTools
-#bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed
-bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed
-#
-# #DNA-spike in normalization
-# #mkdir $OUTDIR/bedgraphs
-sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/DNAspike_in.kd.sh /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed 100000 bga "/scratch/ry00555/OutputRun137/CutandRun/ref/GenomeDir/chrNameLength.txt" 1 1000 /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/${name}.norm.bga
-done
-#sort bga files from  DNA spike in
-   ml ucsc
-   for infile in /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/*norm.bga
-    do
-      base=$(basename ${infile} .norm.bga)
-      bedSort $infile /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/${base}.norm_sort.bga
-   done
-#
- module load Homer
-#  #calling peaks
-  # mkdir $OUTDIR/Peaks
-    for infile in /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/*.norm_sort.bga
-      do base=$(basename ${infile} .norm_sort.bga)
-  cat $infile | awk '{print $1 "\t" $2 "\t" $3 "\t" "+" "\t" "+" "\t" "+"}' > /scratch/ry00555/OutputRun137/CutandRun/Peaks/${base}.bgato.bed
-   done
+# #mkdir $OUTDIR/bed_files
+# for f in $FILES
+# do
+# name=$(basename "${f}" _R1_001_val_1.fq.gz)
 # #
-#    for infile in /scratch/ry00555/OutputRun137/CutandRun/Peaks/*bgato.bed
-#   do base=$(basename ${infile} .bgato.bed)
-#      makeTagDirectory /scratch/ry00555/OutputRun137/CutandRun/TagDirectories/${base}.BtB.tagdir $infile -format bed
-#   done
+# ml BEDTools
+#bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed
+# bedtools bamtobed -i /scratch/ry00555/OutputRun137/CutandRun/SortedBamFiles/${name}_Ecoli.sorted.bam | awk -v OFS='\t' '{len = $3 - $2; print $0, len }' > /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed
+# #
+# # #DNA-spike in normalization
+# # #mkdir $OUTDIR/bedgraphs
+# sh /home/ry00555/Research/FungiCutAndRun/CUTandRUNAnalysis/DNAspike_in.kd.sh /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}.btb.bed /scratch/ry00555/OutputRun137/CutandRun/bed_files/${name}_Ecoli.btb.bed 100000 bga "/scratch/ry00555/OutputRun137/CutandRun/ref/GenomeDir/chrNameLength.txt" 1 1000 /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/${name}.norm.bga
+# done
+# #sort bga files from  DNA spike in
+#    ml ucsc
+#    for infile in /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/*norm.bga
+#     do
+#       base=$(basename ${infile} .norm.bga)
+#       bedSort $infile /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/${base}.norm_sort.bga
+#    done
+# #
+module load Homer
+# #  #calling peaks
+#   # mkdir $OUTDIR/Peaks
+#     for infile in /scratch/ry00555/OutputRun137/CutandRun/bedgraphs/*.norm_sort.bga
+#       do base=$(basename ${infile} .norm_sort.bga)
+#   cat $infile | awk '{print $1 "\t" $2 "\t" $3 "\t" "+" "\t" "+" "\t" "+"}' > /scratch/ry00555/OutputRun137/CutandRun/Peaks/${base}.bgato.bed
+#    done
+# #
+for infile in /scratch/ry00555/OutputRun137/CutandRun/Peaks/*bgato.bed
+do base=$(basename ${infile} .bgato.bed)
+ makeTagDirectory /scratch/ry00555/OutputRun137/CutandRun/TagDirectories/${base}.BtB.tagdir $infile -format bed
+   done
 
 #!/bin/bash
 
@@ -108,7 +108,7 @@ done
 # # this code works where I have to type in strain  so do it for the rest change rtt109 to WT , set-7, ncu00423, ncu006787, ncu06788
 #
 # ##using IgG as input
-# for infile in $OUTDIR/TagDirectories/*WT*.tagdir
+#for infile in $OUTDIR/TagDirectories/*WT*.tagdir
 # do
 #   base=$(basename ${infile} .tagdir)
 # findPeaks $infile -style histone -minDist 1000 -i $OUTDIR/TagDirectories/137-1_CUTANDRUN_WT_IgG_Rep1.BtB.tagdir -F 4 -gsize 4.5e7 -o $OUTDIR/Peaks/${base}_IgGNorm.txt
