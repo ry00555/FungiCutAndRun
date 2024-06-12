5006ef3d1c1db6ac247535566fa086bb9eb19592
ry00555 <111528595+ry00555@users.noreply.github.com>
2024-06-12 10:01:52 -0400

    Run139

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -16,7 +16,7 @@ cd $SLURM_SUBMIT_DIR
 
 source config.txt
 
-OUTDIR="/scratch/ry00555/OutputRun111"
+OUTDIR="/scratch/ry00555/OutputRun139"
 
 
   mkdir "${OUTDIR}/TrimmedReads"
@@ -35,7 +35,7 @@ BEDDIR="${OUTDIR}/Beds"
 # # #process reads using trimGalore
 # #
   ml Trim_Galore
-  trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*ChIP*fastq\.gz
+  trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 # #
  FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
 # #
@@ -52,7 +52,7 @@ for f in $FILES
 #
  	file=${f##*/}
 # 	#remove ending from file name to create shorter names for bam files and other downstream output
- 	name=${file/%_S[1-12]*_R1_001_val_1.fq.gz/}
+ 	name=${file/%_S[1-99]*_R1_001_val_1.fq.gz/}
 #
 # #
 # # 	# File Vars
@@ -62,7 +62,7 @@ for f in $FILES
 bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 # 	#variable name for bigwig output
  	bigwig="${OUTDIR}/BigWigs/${name}"
-# 	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
 # #
 #
 ml SAMtools
@@ -71,8 +71,8 @@ ml BWA
  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
  samtools index "$bam"
 #
-# #samtools view -b -q 30 $bam > "$QualityBam"
-# #samtools index "$QualityBam"
+samtools view -b -q 30 $bam > "$QualityBam"
+samtools index "$QualityBam"
 #
 # ############################
 # # # #deeptools
@@ -82,44 +82,57 @@ ml BWA
  bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 #
  #plot mononucleosomes
- bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
+ bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$QualityBam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_Q30.bw"
+done
+mkdir $OUTDIR/MACSPeaks
+PEAKDIR="${OUTDIR}/MACSPeaks"
+
+ module load MACS3
+ #command line
+ #macs3 callpeak -t 137-11_CUTANDRUN_rtt109_H3K36me3_Rep1_S11_Ecoli.sorted.bam -f BAMPE -n 137-11_CUTANDRUN_rtt109_H3K36me3_Rep1_S11_Ecoli -c 137-9_CUTANDRUN_rtt109_IgG_Rep1_S9_Ecoli.sorted.bam --broad -g 41037538 --broad-cutoff 0.1 --outdir /scratch/ry00555/OutputRun137/CutandRun/MACSPeaks --min-length 800 --max-gap 500
+
+ for infile in $QualityBam
+  do
+   base=$(basename ${infile} _Q30.bam)
+ macs3 callpeak -t $infile -f BAMPE -n $base -c $QualityBam --broad -g 41037538 --broad-cutoff 0.1 --outdir $PEAKDIR --min-length 800 --max-gap 500
  done
 
-ml Homer
-ml Perl
-ml SAMtools
-ml BEDTools
-for bam_file in "${BAMDIR}"/*.bam; do
-  # Get the sample ID from the BAM file name
-  sample_id=$(basename "${bam_file}" .bam)
-  # Remove everything after "Rep_1" in the sample ID
-  sample_id="${sample_id%%_Rep_1*}"
 
-
-#makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
+# ml Homer
+# ml Perl
+# ml SAMtools
+# ml BEDTools
+# for bam_file in "${BAMDIR}"/*.bam; do
+#   # Get the sample ID from the BAM file name
+#   sample_id=$(basename "${bam_file}" .bam)
+#   # Remove everything after "Rep_1" in the sample ID
+#   sample_id="${sample_id%%_Rep_1*}"
 #
-#   # Call peaks
 #
-findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}_peaks.txt"
+# #makeTagDirectory "${TAGDIR}/${sample_id}" "${bam_file}"
+# #
+# #   # Call peaks
+# #
+# findPeaks "${TAGDIR}/${sample_id}" -style histone -region -size 150 -minDist 530 -o "${PEAKDIR}/${sample_id}_peaks.txt"
+# #
+# done
+# #changing peak txt files to bed files to input into chipr
+# ml ChIP-R
+#  for infile in ${PEAKDIR}/${sample_id}_peaks.txt
+# do
+#   base=$(basename ${infile} .txt)
+#   sed '/^#/d' $infile | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $8 "\t" $5 "\t" $6 "\t" $12 "\t" "-1"}' | sed 's/\.000000//g' > ${PEAKDIR}/${base}.peaks.bed
+# done
 #
-done
-#changing peak txt files to bed files to input into chipr
-ml ChIP-R
- for infile in ${PEAKDIR}/${sample_id}_peaks.txt
-do
-  base=$(basename ${infile} .txt)
-  sed '/^#/d' $infile | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $8 "\t" $5 "\t" $6 "\t" $12 "\t" "-1"}' | sed 's/\.000000//g' > ${PEAKDIR}/${base}.peaks.bed
-done
-
-ml Homer
-ml Perl
-##annotating peak files with masked reference (use HOMER module)
-#curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/182/925/GCF_000182925.2_NC12/GCF_000182925.2_NC12_genomic.gtf.gz | gunzip -c > Ncrassa_refann.gtf
- annotatePeaks.pl ${PEAKDIR}/${base}.peaks.bed -gtf scratch/ry00555/Ncrassa_refann.gtf > ${PEAKDIR}/${base}_ann.txt
-
-#now filtering for only peaks that are w/i 1000bps of their annotation:
- for infile in ${PEAKDIR}/${base}_ann.txt
- do
-   base=$(basename ${infile} _masked_ann.txt)
-   awk -F'\t' 'sqrt($10*$10) <=1000' $infile > ${PEAKDIR}/${base}.1000bp_ann.txt
- done
+# ml Homer
+# ml Perl
+# ##annotating peak files with masked reference (use HOMER module)
+# #curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/182/925/GCF_000182925.2_NC12/GCF_000182925.2_NC12_genomic.gtf.gz | gunzip -c > Ncrassa_refann.gtf
+#  annotatePeaks.pl ${PEAKDIR}/${base}.peaks.bed -gtf scratch/ry00555/Ncrassa_refann.gtf > ${PEAKDIR}/${base}_ann.txt
+#
+# #now filtering for only peaks that are w/i 1000bps of their annotation:
+#  for infile in ${PEAKDIR}/${base}_ann.txt
+#  do
+#    base=$(basename ${infile} _masked_ann.txt)
+#    awk -F'\t' 'sqrt($10*$10) <=1000' $infile > ${PEAKDIR}/${base}.1000bp_ann.txt
+#  done
