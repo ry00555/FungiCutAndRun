2cecf00655ab6ebb9cc52c524c64aafc974c7272
ry00555 <ry00555@uga.edu>
2024-09-06 19:13:26 -0400

    try the new mappign script

---
+++ submitMappingJob.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#check for required command line argument
+
+if [[ -z "$1" ]]
+then
+		echo "
+		################
+		ERROR: You must include the path to your accession file in the command line call.
+		eg. sh submit.sh Path/To/Your/AccessionFile.txt
+		##################
+		"
+
+		exit
+fi
+
+#iterates through list of accessions and passes to mapping script
+
+fastqPath="/scratch/ry00555/OutputRun142/2024_Run142_FASTQ"
+outdir="/scratch/ry00555/OutputRun142/"
+
+mkdir ${outdir}
+mkdir ${outdir}/logs
+
+#make output file folders
+mkdir "${outdir}/TrimmedReads"
+mkdir "${outdir}/SortedBamFiles"
+mkdir "${outdir}/BigWigs"
+mkdir "${outdir}/MACSPeaks"
+
+
+while read -r line
+
+	do
+	sleep 10
+	echo "$line mapping job submitted"
+	sbatch --job-name="${line}" --export=ALL,accession="${line}",fastqPath="${fastqPath}",outdir="${outdir}" MapData.sh & done <"$1"
