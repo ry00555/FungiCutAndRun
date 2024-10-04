9e0765ca23f22a1e7fc016f0f62fa879b53b7aaf
ry00555 <ry00555@uga.edu>
2024-10-04 14:26:05 -0400

    Update RNASeqBamCoverage.sh

--- RNASeqBamCoverage.sh
+++ RNASeqBamCoverage.sh
@@ -31,13 +31,12 @@ source config.txt
 
 
 # List of SRR IDs
-# #SRR_IDS=(
-#     "SRR9027634" "SRR9027635" "SRR9027636" "SRR9027653" "SRR9027655"
-#     "SRR9027701" "SRR9044213" "SRR9044244" "SRR9044324" "SRR10916182"
-#     "SRR10916183" "SRR10916184" "SRR10916163" "SRR10916164" "SRR10916165"
-#     "SRR8444005" "SRR8444042" "SRR8443998" "SRR12614222" "SRR12614223"
-#     "SRR12614224" "SRR12614225" "SRR12614226"
-# )
+SRR_IDS=(
+     "SRR9027634" "SRR9027635" "SRR9027636" "SRR9027653" "SRR9027655"
+     "SRR9027701" "SRR9044213" "SRR9044244" "SRR9044324" "SRR10916182"
+     "SRR10916183" "SRR10916184" "SRR10916163" "SRR10916164" "SRR10916165"
+     "SRR8444005" "SRR8444042" "SRR8443998" "SRR12614222" "SRR12614223"
+     "SRR12614224" "SRR12614225" "SRR12614226")
 #
 # # Prefetch SRA files
 #for SRR_ID in "${SRR_IDS[@]}"; do
@@ -77,86 +76,139 @@ source config.txt
  ml BWA
 ml SAMtools
 ml Trim_Galore
- trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+ #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
  #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/SRR12614227*fastq\.gz
  #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/SRR12614226*fastq\.gz
 
 
 #
-FILES="${OUTDIR}/TrimmedReads/*.fq.gz" #Don't forget the *
+#FILES="${OUTDIR}/TrimmedReads/*.fq.gz" #Don't forget the *
 # #
 #
 # #
 # #Iterate over the files
-for f in $FILES
-do
+#for f in $FILES#
+#do
 #
 # 	#Examples to Get Different parts of the file name
 # 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
 		#${string//substring/replacement}
 # 		#dir=${f%/*}
 
-	file=${f##*/}
-	#remove ending from file name to create shorter names for bam files and other downstream output
-	name=${file/*_1.fq.gz/}
+# 	file=${f##*/}
+# 	#remove ending from file name to create shorter names for bam files and other downstream output
+# 	name=${file/*_1.fq.gz/}
+#
+# #
+# # 	# File Vars
+# # 	#use sed to get the name of the second read matching the input file
+# 	read2=$(echo "$f" | sed 's/_1\.fq\.gz/_2\.fq\.gz/g')
+# 	#variable for naming bam file
+#  	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+# 	#variable name for bigwig output
+# 	bigwig="${OUTDIR}/BigWigs/${name}"
+# 	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+# #
+#
+# #
+# bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+# samtools index "$bam"
+# #
+# # #samtools view -b -q 30 $bam > "$QualityBam"
+# # #samtools index "$QualityBam"
+# #
+# # ############################
+# # # #deeptools
+#
+# ml deepTools
+# bamCoverage -p $THREADS -bs $BIN --normalizeUsing CPM --ignoreDuplicates --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+# #
+#  done
+#
 
-#
-# 	# File Vars
-# 	#use sed to get the name of the second read matching the input file
-	read2=$(echo "$f" | sed 's/_1\.fq\.gz/_2\.fq\.gz/g')
-	#variable for naming bam file
- 	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
-	#variable name for bigwig output
-	bigwig="${OUTDIR}/BigWigs/${name}"
-	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
-#
+Wildtype
 
-#
-bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-samtools index "$bam"
-#
-# #samtools view -b -q 30 $bam > "$QualityBam"
-# #samtools index "$QualityBam"
-#
-# ############################
-# # #deeptools
+SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+NCU07496
 
-ml deepTools
-bamCoverage -p $THREADS -bs $BIN --normalizeUsing CPM --ignoreDuplicates --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
-#
- done
-#
-# for file_path in "${OUTDIR}/BigWigs"/*.bw; do
+SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+NCU06787
+
+SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+NCU06788
+
+SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+
+
+"../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+"../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw"
+
+Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6
+for file_path in "${OUTDIR}/BigWigs"/*.bw; do
 #   # Get the base name of the file
-#   BW_name=$(basename "${file_path}" .bw)
+   BW_name=$(basename "${file_path}" .bw)
 #
 #   # Remove the file extension to create the sample ID
-#   BW_id="${BW_name%.*}"
+   BW_id="${BW_name%.*}"
 #   # Replace special characters with underscores in the sample ID
-#  BW_id=${BW_id//[^a-zA-Z0-9]/_}
+  BW_id=${BW_id//[^a-zA-Z0-9]/_}
 #
 #  # Limit the length of the sample ID to avoid long filenames
-#  BW_id=${BW_id:0:50}
+  BW_id=${BW_id:0:50}
 #  # Compute matrix for the reference-point TSS
-#  computeMatrix reference-point --referencePoint TSS -b 1500 -a 1500 -S "${file_path}" -R "/scratch/ry00555/neurospora.bed" --skipZeros -o "${OUTDIR}/Matrices/matrix_${BW_id}.gz"
-#
+  computeMatrix scale-regions --startLabel TSS --endLabel TES -a 500 -b 500 --unscaled5prime 500 --unscaled3prime 500 -S ../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw -R /scratch/ry00555/neurospora.bed --skipZeros --missingDataAsZero -o EAF3_V3.gz
+plotHeatmap --matrixFile ../Matrices/EAF3_V3.gz --outFileName EAF3_processed_V3_regions_hclust.png --samplesLabel Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --regionsLabel "CM002236.1" "CM002237.1" "CM002238.1" "CM002239.1" "CM002240.1" "CM002241.1" "CM002242.1" --colorMap spring summer autumn winter --missingDataColor white --sortUsingSamples 1 2 3
+
+plotHeatmap --matrixFile ../Matrices/EAF3_V3.gz --outFileName EAF3_processed_V3_hclust.png --samplesLabel Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --colorMap spring summer autumn winter --sortRegions descend --missingDataColor white --sortUsingSamples 1 2 3
+
+#Remove ../BigWigs/SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw bc is bad WT
+
+computeMatrix scale-regions -p 12 --startLabel TSS --endLabel TES -a 1000 -b 1000 --unscaled5prime 500 --unscaled3prime 500 -S ../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw -R /scratch/ry00555/neurospora.bed --skipZeros --missingDataAsZero -o EAF3_V4.gz
+
+plotHeatmap --matrixFile ../Matrices/EAF3_V4.gz --outFileName EAF3_processed_V6_wholeGenome_hclust.png --samplesLabel Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --sortRegions descend --colorMap coolwarm --missingDataColor white --sortUsingSamples 1 2 --zMax auto --zMin auto --heatmapHeight 12 --whatToShow 'heatmap and colorbar'
+
+  #zcat EAF3_V2.gz | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > EAF3_processed_V2.gz
+
+
 #   # Compute matrix for the reference-point TSS with specific regions (e.g., PRC2 genes)
 #   computeMatrix reference-point --referencePoint TSS -b 1500 -a 1500 -S "${file_path}" -R "/scratch/ry00555/heatmapPRC2genes.bed" --skipZeros -o "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz"
-#
+computeMatrix scale-regions -p 12  --startLabel TSS --endLabel TES -a 1000 -b 1000 -S ../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw ../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw -R /scratch/ry00555/heatmapPRC2genes.bed --skipZeros -o EAF3_K27me3Domains2_V3.gz
+
+plotHeatmap --matrixFile EAF3_K27me3Domains2_V3.gz --outFileName ../Heatmaps/EAF3_processed_V5_K27me3regions_hclust.png --samplesLabel Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --sortRegions descend --colorMap coolwarm --missingDataColor white --sortUsingSamples 1 2 --zMax auto --zMin auto --heatmapHeight 12 --whatToShow 'heatmap and colorbar'
+
+zcat EAF3_K27me3Domains2_V2.gz| awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > EAF3_K27me3Domains2_processed_V2.gz
+
 #   # Preprocess the matrix files to replace nan values with zeros
-#   zcat "${OUTDIR}/Matrices/matrix_${BW_id}.gz" | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > "${OUTDIR}/Matrices/matrix_${BW_id}_processed.gz"
-#   zcat "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz" | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}_processed.gz"
+   zcat "${OUTDIR}/Matrices/matrix_${BW_id}_V2.gz" | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > "${OUTDIR}/Matrices/matrix_${BW_id}_processed_V2.gz"
+   zcat "${OUTDIR}/Matrices/matrix_${BW_id}_K27me3Domains2_V2.gz" | awk '{for (i=1; i<=NF; i++) if ($i == "nan") $i=0; print}' | gzip > "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}_processed_V2.gz"
 #
 #   # Plot heatmaps for the reference-point TSS
-#  plotHeatmap --matrixFile "${OUTDIR}/Matrices/matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Greens --sortRegions descend --missingDataColor white --sortUsingSamples 1
+ plotHeatmap --matrixFile ../Matrices/EAF3_K27me3Domains2_processed_V2.gz --outFileName EAF3_K27me3Domains2_processed_V2_hclust.png --samplesLabel Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --colorMap Reds --sortRegions descend --missingDataColor white --sortUsingSamples 1 2 3
 #
 #   # Plot heatmaps for the reference-point TSS with specific regions (e.g., PRC2 genes)
-#  plotHeatmap --matrixFile "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust_PRC2genes.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Greens
+# plotHeatmap --matrixFile "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust_PRC2genes.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Greens
 #
 #  plotHeatmap --matrixFile "${OUTDIR}/Matrices/matrix_${BW_id}_processed.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Greens --sortRegions descend --missingDataColor white --sortUsingSamples 1
 #
 #   # Plot heatmaps for the reference-point TSS with specific regions (e.g., PRC2 genes)
-#  plotHeatmap --matrixFile "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}_processed.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust_PRC2genes.png" --samplesLabel "${BW_name}" --hclust 1 --colorMap Greens
+ plotHeatmap --matrixFile "${OUTDIR}/Matrices/PRC2genes_matrix_${BW_id}_processed_V2.gz" --outFileName "${OUTDIR}/Heatmaps/${BW_id}_hclust_PRC2genes_ZLMethod_V2.png" --samplesLabel Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6 --hclust 1 --colorMap Reds --sortRegions descend --missingDataColor white --sortUsingSamples 1 2 3
 #
-#
-# done
+ done
