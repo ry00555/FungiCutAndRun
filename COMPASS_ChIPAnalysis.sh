6657c04d45dfc6a09ba9ecc7e26c47c3abdbb874
ry00555 <ry00555@uga.edu>
2025-01-13 14:22:13 -0500

    Update COMPASS_ChIPAnalysis.sh

--- COMPASS_ChIPAnalysis.sh
+++ COMPASS_ChIPAnalysis.sh
@@ -122,11 +122,12 @@ ml Perl
 #findPeaks ${TAGDIR}/145-30_ChIP_WT_H3K27me3 -style histone -region -size 150 -minDist 530 -o ${HOMERPEAKSDIR}/145-30_ChIP_WT_H3K27me3_Homerpeaks.txt -i ${TAGDIR}/145-29_ChIP_WT_Input
 #findPeaks ${TAGDIR}/142-94_ChIP_set7_H3K27me3  -style histone -region -size 150 -minDist 530 -o ${HOMERPEAKSDIR}/142-94_ChIP_set7_H3K27me3_Homerpeaks.txt -i ${TAGDIR}/142-93_ChIP_set7_Input
 Genome="/home/zlewis/Genomes/Neurospora/Nc12_RefSeq/GCA_00182925.2plusHphplusBarplusTetO_his3masked.fna"
+GTF="/home/zlewis/Genomes/Neurospora/Nc12_RefSeq/GCA_000182925.2_NC12_genomic_HOMER.gtf"
  for infile in ${HOMERPEAKSDIR}/*_Homerpeaks.txt
 do
   base=$(basename ${infile} _Homerpeaks.txt)
   sed '/^#/d' $infile | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $8 "\t" $5 "\t" $6 "\t" $12 "\t" "-1"}' | sed 's/\.000000//g' > ${HOMERPEAKSDIR}/${base}.peaks.bed
-# annotatePeaks.pl ${HOMERPEAKSDIR}/${base}.peaks.bed $Genome -gtf "/scratch/ry00555/Ncrassa.gtf" > ${HOMERPEAKSDIR}/${base}_ann.txt
+ annotatePeaks.pl ${HOMERPEAKSDIR}/${base}.peaks.bed $Genome -gtf $GTF > ${HOMERPEAKSDIR}/${base}_ann.txt
 
 done
 
