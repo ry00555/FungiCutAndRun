24ba4fa7446fb506e955f3eab19df48c932f7cf3
ry00555 <ry00555@uga.edu>
2025-03-21 15:14:17 -0400

    Run147 ChIP Dahlstrom

--- ChIPforDahlstrom.sh
+++ ChIPforDahlstrom.sh
@@ -7,20 +7,20 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=50gb
 #SBATCH --time=48:00:00
-#SBATCH --output=../DahlstrohmChIP.%j.out
-#SBATCH --error=../DahlstrohmChIP.%j.err
+#SBATCH --output=../DahlstrohmChIPRun147.%j.out
+#SBATCH --error=../DahlstrohmChIPRun147.%j.err
 
 cd $SLURM_SUBMIT_DIR
 
-#ml BWA/0.7.17-GCCcore-11.3.0
-#ml SAMtools/0.1.20-GCC-11.3.0
+ml BWA/0.7.17-GCCcore-11.3.0
+ml SAMtools/0.1.20-GCC-11.3.0
 #If incompatible modules use SAMtools/1.16.1-GCC-11.3.0 instead rec by Chelsea Taylor
 ml deepTools/3.5.2-foss-2022a
 
 
 ml Homer/4.11-foss-2022a
 ml Perl/5.34.1-GCCcore-11.3.0
-#ml Trim_Galore/0.6.7-GCCcore-11.2.0
+ml Trim_Galore/0.6.7-GCCcore-11.2.0
 #ml Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 #ml BEDTools/2.30.0-GCC-11.3.0
 #read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
@@ -28,7 +28,7 @@ ml Perl/5.34.1-GCCcore-11.3.0
 
 source ConfigforDahlstrom.txt
 
-OUTDIR=/scratch/ry00555/DahlstromRun135/Output
+OUTDIR="/scratch/ry00555/Run147/Dahlstrom"
 #if [ ! -d $OUTDIR ]
 #then
 #mkdir -p $OUTDIR
@@ -37,9 +37,9 @@ OUTDIR=/scratch/ry00555/DahlstromRun135/Output
 
 # #process reads using trimGalore
 
- #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+ trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 #
-#FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
+FILES="${OUTDIR}/TrimmedReads/*R1_001_val_1\.fq\.gz" #Don't forget the *
 #
 #mkdir "${OUTDIR}/SortedBamFiles"
 #mkdir "${OUTDIR}/BigWigs"
@@ -48,32 +48,32 @@ OUTDIR=/scratch/ry00555/DahlstromRun135/Output
 #mkdir "$OUTDIR/TdfFiles"
 #
 #Iterate over the files
-#for f in $FILES
-#do
+for f in $FILES
+do
 #
 # 	#Examples to Get Different parts of the file name
 # 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
 		#${string//substring/replacement}
 # 		#dir=${f%/*}
 
-	#file=${f##*/}
+	file=${f##*/}
 	#remove ending from file name to create shorter names for bam files and other downstream output
-	#name=${file/%_S[1-100]*_R1_001_val_1.fq.gz/}
+	name=${file/%_S[1-100]*_R1_001_val_1.fq.gz/}
 
 #
 # 	# File Vars
 # 	#use sed to get the name of the second read matching the input file
-	#read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
+	read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
 	#variable for naming bam file
- 	#bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+ 	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 	#variable name for bigwig output
-	#bigwig="${OUTDIR}/BigWigs/${name}"
+	bigwig="${OUTDIR}/BigWigs/${name}"
 	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
 #
 
 #
-#bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-#samtools index "$bam"
+bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+samtools index "$bam"
 
 #samtools view -b -q 30 $bam > "$QualityBam"
 #samtools index "$QualityBam"
@@ -82,12 +82,12 @@ OUTDIR=/scratch/ry00555/DahlstromRun135/Output
 # # #deeptools
 
 #Plot all reads
-#bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 
 #plot mononucleosomes
 #bamCoverage -p $THREADS --MNase -bs 1 --normalizeUsing BPM --smoothLength 25 -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_MNase.bw"
 
-#done
+done
 
 
 
@@ -139,6 +139,6 @@ PEAKSDIR="${OUTDIR}/Peaks"
 #findPeaks "${TAGDIR}/135-43_ChIP_hcra6xhis_Anti6xHis_Rep1_S40" -style factor -o "${PEAKSDIR}/135-43_ChIP_hcra6xhis_Anti6xHis_Rep1_S40_peaks.txt"
 #-i "${TAGDIR}/135-46_ChIP_hcra6xhis_Input_Rep1_S43"
 
-annotatePeaks.pl "${PEAKSDIR}/135-41_ChIP_WT_Anti6xHis_Rep1_S38_peaks_wINPUT.txt" $GENOME -gtf /home/ry00555/Research/Genomes/genomic.gtf > ${OUTDIR}/135-41_ChIP_WT.txt
+#annotatePeaks.pl "${PEAKSDIR}/135-41_ChIP_WT_Anti6xHis_Rep1_S38_peaks_wINPUT.txt" $GENOME -gtf /home/ry00555/Research/Genomes/genomic.gtf > ${OUTDIR}/135-41_ChIP_WT.txt
 
 #annotatePeaks.pl "${PEAKSDIR}/135-43_ChIP_hcra6xhis_Anti6xHis_Rep1_S40_peaks.txt" $GENOME -gtf /home/ry00555/Research/Genomes/GCA_019428685.1_ASM1942868v1_genomic.gtf > ${OUTDIR}/135-43_hrcA6xhis.txt
