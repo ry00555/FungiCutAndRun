7a138ff517b7e61b6869be881fd1d948d904b617
ry00555 <111528595+ry00555@users.noreply.github.com>
2025-09-15 11:08:34 -0400

    Update RNASeqBamCoverage.sh

--- RNASeqBamCoverage.sh
+++ RNASeqBamCoverage.sh
@@ -19,13 +19,13 @@ cd $SLURM_SUBMIT_DIR
 source config.txt
 
 #if output directory doesn't exist, create it
-# if [ ! -d $OUTDIR ]
-# then
-#     mkdir -p $OUTDIR
-# fi
+ if [ ! -d $OUTDIR ]
+ then
+     mkdir -p $OUTDIR
+ fi
 
 #loading modules
-#ml SRA-Toolkit
+ml SRA-Toolkit
 
 #You can use a loop to prefetch each SRR ID and then subsequently run fastq-dump for each downloaded file. Here's how you can do it:
 
@@ -39,127 +39,127 @@ SRR_IDS=(
      "SRR12614224" "SRR12614225" "SRR12614226")
 #
 # # Prefetch SRA files
-#for SRR_ID in "${SRR_IDS[@]}"; do
-#    prefetch -O "${OUTDIR}" "${SRR_ID}"
-#done
+for SRR_ID in "${SRR_IDS[@]}"; do
+    prefetch -O "${OUTDIR}" "${SRR_ID}"
+done
 
 #downloading reference genome
 #curl -s ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/fungi/Neurospora_crassa/latest_assembly_versions/GCA_000182925.2_NC12/GCA_000182925.2_NC12_genomic.fna.gz  | gunzip -c > ${OUTDIR}/NC12_genome.fna
 #curl -s ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/182/925/GCA_000182925.2_NC12/GCA_000182925.2_NC12_genomic.gtf.gz | gunzip -c > ${OUTDIR}/NC12_annotation.gtf
 
 #DOWNLOADING SRA
-#prefetch -O SRR9027634 SRR9027635 SRR9027636 SRR9027653 SRR9027655 SRR9027701 SRR9044213 SRR9044244 SRR9044324 SRR10916182 SRR10916183 SRR10916184 SRR10916163 SRR10916164 SRR10916165 SRR8444005 SRR8444042 SRR8443998 SRR12614222 SRR12614223 SRR12614224 SRR12614225 SRR12614226
+prefetch -O SRR9027634 SRR9027635 SRR9027636 SRR9027653 SRR9027655 SRR9027701 SRR9044213 SRR9044244 SRR9044324 SRR10916182 SRR10916183 SRR10916184 SRR10916163 SRR10916164 SRR10916165 SRR8444005 SRR8444042 SRR8443998 SRR12614222 SRR12614223 SRR12614224 SRR12614225 SRR12614226
 
 ##convert to fastq
-# for SRR_ID in "${SRR_IDS[@]}"; do
-# fastq-dump --split-files --gzip ${OUTDIR}/${SRR_ID}/${SRR_ID}.sra -O ${FASTQ}
-# done
+ for SRR_ID in "${SRR_IDS[@]}"; do
+ fastq-dump --split-files --gzip ${OUTDIR}/${SRR_ID}/${SRR_ID}.sra -O ${FASTQ}
+ done
 #single commmand
 # fastq-dump --split-files --gzip	${OUTDIR}/SRR9027634
 #
 
 #Finish the reminaing SRR's as the script was interrupted halfway due to GACRC maintainence
-#ml SRA-Toolkit
+ml SRA-Toolkit
 #fastq-dump --split-files --gzip "${OUTDIR}/SRR12614226/SRR12614226.sra" -O ${OUTDIR}/FASTQ
 #fastq-dump --split-files --gzip "${OUTDIR}/SRR12614227/SRR12614227.sra" -O ${OUTDIR}/FASTQ
 
 # #process reads using trimGalore
 
 #
-#mkdir "${OUTDIR}/SortedBamFiles"
-#mkdir "${OUTDIR}/BigWigs"
-#mkdir "${OUTDIR}/Peaks"
-#mkdir "${OUTDIR}/TrimmedReads"
-#mkdir "$OUTDIR/Matrices"
-#mkdir "$OUTDIR/Heatmaps"
+mkdir "${OUTDIR}/SortedBamFiles"
+mkdir "${OUTDIR}/BigWigs"
+mkdir "${OUTDIR}/Peaks"
+mkdir "${OUTDIR}/TrimmedReads"
+mkdir "$OUTDIR/Matrices"
+mkdir "$OUTDIR/Heatmaps"
 
  ml BWA
 ml SAMtools
 ml Trim_Galore
- #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+ trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
  #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/SRR12614227*fastq\.gz
  #trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/SRR12614226*fastq\.gz
 
 
 #
-#FILES="${OUTDIR}/TrimmedReads/*.fq.gz" #Don't forget the *
+FILES="${OUTDIR}/TrimmedReads/*.fq.gz" #Don't forget the *
 # #
 #
 # #
 # #Iterate over the files
-#for f in $FILES#
-#do
+for f in $FILES#
+do
 #
 # 	#Examples to Get Different parts of the file name
 # 		#See here for details: http://tldp.org/LDP/abs/html/refcards.html#AEN22664
 		#${string//substring/replacement}
 # 		#dir=${f%/*}
 
-# 	file=${f##*/}
+	file=${f##*/}
 # 	#remove ending from file name to create shorter names for bam files and other downstream output
-# 	name=${file/*_1.fq.gz/}
+ 	name=${file/*_1.fq.gz/}
 #
 # #
 # # 	# File Vars
 # # 	#use sed to get the name of the second read matching the input file
-# 	read2=$(echo "$f" | sed 's/_1\.fq\.gz/_2\.fq\.gz/g')
+ 	read2=$(echo "$f" | sed 's/_1\.fq\.gz/_2\.fq\.gz/g')
 # 	#variable for naming bam file
-#  	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+  	bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 # 	#variable name for bigwig output
-# 	bigwig="${OUTDIR}/BigWigs/${name}"
-# 	#QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
+ 	bigwig="${OUTDIR}/BigWigs/${name}"
+ QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
 # #
 #
 # #
-# bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-# samtools index "$bam"
+ bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+ samtools index "$bam"
 # #
-# # #samtools view -b -q 30 $bam > "$QualityBam"
-# # #samtools index "$QualityBam"
+samtools view -b -q 30 $bam > "$QualityBam"
+samtools index "$QualityBam"
 # #
 # # ############################
 # # # #deeptools
 #
-# ml deepTools
-# bamCoverage -p $THREADS -bs $BIN --normalizeUsing CPM --ignoreDuplicates --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+ ml deepTools
+ bamCoverage -p $THREADS -bs $BIN --normalizeUsing CPM --ignoreDuplicates --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 # #
-#  done
+  done
 #
 
-Wildtype
+#Wildtype
 
-SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-NCU07496
+#SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#NCU07496
 
-SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-NCU06787
+#SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#NCU06787
 
-SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-NCU06788
+#SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#NCU06788
 
-SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
-SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
+#SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw
 
 
-"../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
-"../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw"
+#"../BigWigs/SRR8444005_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR8444042_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR8443998_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR10916163_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR10916164_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR10916165_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9044213_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9044244_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9044324_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9027653_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9027655_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw",
+#"../BigWigs/SRR9027701_2_val_2.fq.gz.bin_25.smooth_50Bulk.bw"
 
 Wildtype Wildtype Wildtype ∆set-7 ∆set-7 ∆set-7 ∆mrg15 ∆mrg15 ∆mrg15 ∆cdp-6 ∆cdp-6 ∆cdp-6
 for file_path in "${OUTDIR}/BigWigs"/*.bw; do
