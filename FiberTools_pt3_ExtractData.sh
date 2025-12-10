09695bc96fe6e8022dbc54b0a22e6448cc959c95
ry00555 <111528595+ry00555@users.noreply.github.com>
2025-12-10 17:48:56 -0500

    Update FiberTools_pt3_ExtractData.sh

--- FiberTools_pt3_ExtractData.sh
+++ FiberTools_pt3_ExtractData.sh
@@ -15,12 +15,30 @@ module load Miniforge3/24.11.3-0
 source activate /home/ry00555/fibertools
 
 WORKDIR="/lustre2/scratch/ry00555/ONTRun9_10Combined/fibertools_results"
-mkdir -p "$OUT_DIR"
 THREADS=8
+GENOME="/home/ry00555/Research/Genomes/GenBankNcrassachromsizes.txt"
+TSS_BED="/home/ry00555/Research/Genomes/neurospora.bed"
+ml ucsc
 
 for BAM in "$WORKDIR"/*_merged.nucs.bam; do
     [ -f "$BAM" ] || continue
     SAMPLE=$(basename "$BAM" _merged.nucs.bam)
     echo "Processing $SAMPLE ..."
+    PILEUP="$WORKDIR/${SAMPLE}.pileup.bedgraph"
+    BIGWIG="$WORKDIR/${SAMPLE}.pileup.bw"
+    bedGraphToBigWig "$PILEUP" $GENOME "$BIGWIG" || echo "BigWig conversion failed for $SAMPLE"
 
-    ft extract 
+TSS_MATRIX="$WORKDIR/${SAMPLE}.TSS.matrix.gz"
+computeMatrix reference-point \
+    --referencePoint TSS \
+    -b 2000 -a 1000 \
+    -R "$TSS_BED" \
+    -S "$BIGWIG" \
+    -o "$TSS_MATRIX" \
+    --skipZeros \
+    --numberOfProcessors $THREADS
+
+    plotProfile -m "$TSS_MATRIX" -out "$WORKDIR/${SAMPLE}.TSS_profile.png" \
+        --perGroup --colors red
+
+done
