0b7735d67f929a4aed3159a51ec63cebaf592786
ry00555 <111528595+ry00555@users.noreply.github.com>
2025-12-28 11:28:44 -0500

    Update MapCutAndRun.sh

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -36,40 +36,40 @@ OUTDIR="/scratch/ry00555/Run152"
 
 
 #  Iterate over the files
- for f in $FILES
- do
+# for f in $FILES
+# do
 
 #   	Examples to Get Different parts of the file name
 #   		See here for details: http://tldp.org/LDP/abs/html/refcards.htmlAEN22664
 #  		${string//substring/replacement}
 #   		dir=${f%/*}
 #
-file=${f##*/}
+#file=${f##*/}
 #  	remove ending from file name to create shorter names for bam files and other downstream output
- name=${file/%_S[1-150]*_L006_R1_001_val_1.fq.gz/}
+ #name=${file/%_S[1-150]*_L006_R1_001_val_1.fq.gz/}
 # name=${file/%_S[1-990]*_L002_R1_001_val_1.fq.gz/}
 # name=${file/%_S[1-190]*R1_001_val_1.fq.gz/}
 #
 #
 #  	 File Vars
 #  	use sed to get the name of the second read matching the input file
- read2=$(echo "$f" | sed 's/_L006_R1_001_val_1\.fq\.gz/_L006_R2_001_val_2\.fq\.gz/g')
+ #read2=$(echo "$f" | sed 's/_L006_R1_001_val_1\.fq\.gz/_L006_R2_001_val_2\.fq\.gz/g')
 #
 #   	 File Vars
 #   	use sed to get the name of the second read matching the input file
 # read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
 #  	variable for naming bam file
-bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+#bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 #  	variable name for bigwig output
- bigwig="${OUTDIR}/BigWigs/${name}"
+ #bigwig="${OUTDIR}/BigWigs/${name}"
 #QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
 #
 #
- ml SAMtools/1.16.1-GCC-11.3.0
- ml BWA/0.7.17-GCCcore-11.3.0
+ #ml SAMtools/1.16.1-GCC-11.3.0
+ #ml BWA/0.7.17-GCCcore-11.3.0
 #
-  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-  samtools index "$bam"
+#  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+  #samtools index "$bam"
 #
  #samtools view -b -q 30 $bam > "$QualityBam"
 # samtools index "$QualityBam"
@@ -77,25 +77,25 @@ bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 #
 #    deeptools
 #
- ml deepTools
+# ml deepTools
 #  Plot all reads
- bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+# bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 #
  #bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$QualityBam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_Q30.bw"
- done
-# mkdir $OUTDIR/MACSPeaks
-#PEAKDIR="${OUTDIR}/MACSPeaks"
+# done
+mkdir $OUTDIR/MACSPeaks
+PEAKDIR="${OUTDIR}/MACSPeaks"
 
-#ml MACS3
+ml MACS3
 # command line
 #macs3 callpeak -t 137-11_CUTANDRUN_rtt109_H3K36me3_Rep1_S11_Ecoli.sorted.bam -f BAMPE -n 137-11_CUTANDRUN_rtt109_H3K36me3_Rep1_S11_Ecoli -c 137-9_CUTANDRUN_rtt109_IgG_Rep1_S9_Ecoli.sorted.bam --broad -g 41037538 --broad-cutoff 0.1 --outdir /scratch/ry00555/OutputRun137/CutandRun/MACSPeaks --min-length 800 --max-gap 500
 
-#   for infile in $BAMDIR/*_Q30.bam
-#  do
-#     base=$(basename ${infile} _Q30.bam)
+   for infile in $BAMDIR/*.bam
+  do
+     base=$(basename ${infile} _Q30.bam)
 #    Input=$BAMDIR/ ${infile} Input_Q30.bam
-#   macs3 callpeak -t $infile -f BAMPE -n $base --broad -g 41037538 --broad-cutoff 0.1 --outdir $PEAKDIR --min-length 800 --max-gap 500 -c $Input
-#   done
+  macs3 callpeak -t $infile -f BAMPE -n $base --broad -g 41037538 --broad-cutoff 0.1 --outdir $PEAKDIR --min-length 800 --max-gap 500 #-c $Input
+ done
 #
 # HOMERPEAKSDIR="${OUTDIR}/HomerPeaks"
 #    ml Homer
