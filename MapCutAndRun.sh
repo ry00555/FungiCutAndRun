d74db8221e2d976af165596d1290d0f703763857
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-01-07 14:02:20 -0500

    run 145
    run 145

--- MapCutAndRun.sh
+++ MapCutAndRun.sh
@@ -15,7 +15,7 @@ cd $SLURM_SUBMIT_DIR
 #read in variables from the config file ($threads, $FASTQ, $OUTDIR, )
 
 source config.txt
-OUTDIR="/lustre2/scratch/ry00555/Run147/TrimmedReads/RemapTemp/Missed"
+OUTDIR="/lustre2/scratch/ry00555/Run145"
 
 # if output directory doesn't exist, create it
   mkdir -p $OUTDIR
@@ -29,47 +29,47 @@ OUTDIR="/lustre2/scratch/ry00555/Run147/TrimmedReads/RemapTemp/Missed"
  BAMDIR="${OUTDIR}/SortedBamFiles"
  BEDDIR="${OUTDIR}/Beds"
 #   process reads using trimGalore
-#module load Trim_Galore
-#trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
+module load Trim_Galore
+trim_galore --paired --length 20 --fastqc --gzip -o ${OUTDIR}/TrimmedReads ${FASTQ}/*fastq\.gz
 
- #FILES="${OUTDIR}/TrimmedReads/*_L006_R1_001_val_1\.fq\.gz"
+FILES="${OUTDIR}/TrimmedReads/*_L002_R1_001_val_1\.fq\.gz"
 
 # FILES="${OUTDIR}/TrimmedReads/*_R1_001_val_1\.fq\.gz"
 #  Iterate over the files
-# for f in $FILES
-#do
+ for f in $FILES
+do
 
 #   	Examples to Get Different parts of the file name
 #   		See here for details: http://tldp.org/LDP/abs/html/refcards.htmlAEN22664
 #  		${string//substring/replacement}
 #   		dir=${f%/*}
 #
-#file=${f##*/}
+file=${f##*/}
 #  	remove ending from file name to create shorter names for bam files and other downstream output
 # name=${file/%_S[1-150]*_L001_R1_001_val_1.fq.gz/}
-# name=${file/%_S[1-990]*_L002_R1_001_val_1.fq.gz/}
+ name=${file/%_S[1-990]*_L002_R1_001_val_1.fq.gz/}
  #name=${file/%_S[1-190]*R1_001_val_1.fq.gz/}
 #
 #
 #  	 File Vars
 #  	use sed to get the name of the second read matching the input file
-#read2=$(echo "$f" | sed 's/_L001_R1_001_val_1\.fq\.gz/_L001_R2_001_val_2\.fq\.gz/g')
+read2=$(echo "$f" | sed 's/_L002_R1_001_val_1\.fq\.gz/_L002_R2_001_val_2\.fq\.gz/g')
 #
 #   	 File Vars
 #   	use sed to get the name of the second read matching the input file
 #read2=$(echo "$f" | sed 's/R1_001_val_1\.fq\.gz/R2_001_val_2\.fq\.gz/g')
 #  	variable for naming bam file
-#bam="${OUTDIR}/SortedBamFiles/${name}.bam"
+bam="${OUTDIR}/SortedBamFiles/${name}.bam"
 #  	variable name for bigwig output
-#bigwig="${OUTDIR}/BigWigs/${name}"
+bigwig="${OUTDIR}/BigWigs/${name}"
 #QualityBam="${OUTDIR}/SortedBamFiles/${name}_Q30.bam"
 
 
-#ml SAMtools/1.16.1-GCC-11.3.0
-#ml BWA/0.7.17-GCCcore-11.3.0
+ml SAMtools/1.16.1-GCC-11.3.0
+ml BWA/0.7.17-GCCcore-11.3.0
 
-#  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
-#samtools index "$bam"
+  bwa mem -M -v 3 -t $THREADS $GENOME $f $read2 | samtools view -bhSu - | samtools sort -@ $THREADS -T $OUTDIR/SortedBamFiles/tempReps -o "$bam" -
+samtools index "$bam"
 #
  #samtools view -b -q 30 $bam > "$QualityBam"
 # samtools index "$QualityBam"
@@ -77,12 +77,12 @@ OUTDIR="/lustre2/scratch/ry00555/Run147/TrimmedReads/RemapTemp/Missed"
 #
 #    deeptools
 #
-#ml deepTools
+ml deepTools
 #  Plot all reads
-# bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
+ bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$bam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}Bulk.bw"
 #
  #bamCoverage -p $THREADS -bs $BIN --normalizeUsing BPM --minMappingQuality 10 --smoothLength $SMOOTH -of bigwig -b "$QualityBam" -o "${bigwig}.bin_${BIN}.smooth_${SMOOTH}_Q30.bw"
- #done
+ done
 mkdir $OUTDIR/MACSPeaks
 PEAKDIR="${OUTDIR}/MACSPeaks"
 
