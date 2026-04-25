faeee361c5b3084439d64896b27d80188a7838ee
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-25 15:24:40 -0400

    Describe what changed

---
+++ AlphaFold3_Pool.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=alphafold3_pooled_PPI
+#SBATCH --partition=gpu_p
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=Milan|SapphireRapids
+#SBATCH --mem=60gb
+#SBATCH --time=10:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ry00555@uga.edu
+#SBATCH --array=1-200 # change this to the number of input jsons 
+
+cd $SLURM_SUBMIT_DIR
+
+# Directories
+INPUT_DIR="/lustre2/scratch/ry00555/AlphaFold/input/AF3"
+OUTPUT_DIR="/lustre2/scratch/ry00555/AlphaFold/output/AF3"
+MODEL_DIR="/home/ry00555/Research/FungiCutAndRun/AlphaFold3"
+PUBLIC_DB="/db/AlphaFold3/20241114"
+
+# Pick the JSON file for this array task
+file=$(ls $INPUT_DIR/*.json | awk "NR==${SLURM_ARRAY_TASK_ID}")
+
+singularity exec \
+     --nv \
+     --bind ${INPUT_DIR}:/root/af_input \
+     --bind ${OUTPUT_DIR}:/root/af_output \
+     --bind ${MODEL_DIR}:/root/models \
+     --bind ${PUBLIC_DB}:/root/public_databases \
+     /apps/singularity-images/alphafold-3.0.0.sif \
+     python /app/alphafold/run_alphafold.py \
+     --json_path=/root/af_input/$(basename $file) \
+     --model_dir=/root/models \
+     --db_dir=/root/public_databases \
+     --output_dir=/root/af_output
