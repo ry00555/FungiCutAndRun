4eef101162fc7dd7727dfaf56e3e52648daff305
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-25 12:29:44 -0400

    Describe what changed

--- Alphafold_Step1.sh
+++ Alphafold_Step1.sh
@@ -18,10 +18,10 @@ ml AlphaFold/2.3.2-foss-2023a-CUDA-12.1.1
 export ALPHAFOLD_DATA_DIR=/apps/db/AlphaFold/2.3.1
 WORKDIR="/scratch/ry00555/AlphaFold"
 
-file=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $WORKDIR/input.lst)
+file=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $WORKDIR/FastaforAlphaFold/input.lst)
 
 alphafold \
---run_relax=False \
+--models_to_relax=none \
 --data_dir=$ALPHAFOLD_DATA_DIR \
 --uniref90_database_path=$ALPHAFOLD_DATA_DIR/uniref90/uniref90.fasta \
 --mgnify_database_path=$ALPHAFOLD_DATA_DIR/mgnify/mgy_clusters.fa \
