8469f5b267a92e640bebc303708f249521f0f32a
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-25 12:13:54 -0400

    Describe what changed

--- AlphaFold_Step2.sh
+++ AlphaFold_Step2.sh
@@ -15,26 +15,17 @@
 cd $SLURM_SUBMIT_DIR
 
 ml purge
-
-ml AlphaFold/2.3.1-foss-2022a-CUDA-11.7.0
-export ALPHAFOLD_DATA_DIR=/apps/db/AlphaFold/2.3.1
+ml AlphaFold/2.3.2-foss-2023a-CUDA-12.1.1
+export ALPHAFOLD_DATA_DIR=/db/AlphaFold/2.3.2
 export TF_FORCE_UNIFIED_MEMORY=1
 export XLA_PYTHON_CLIENT_MEM_FRACTION=8
-WORKDIR="/scratch/ry00555/AlphaFold"
 
+WORKDIR="/scratch/ry00555/AlphaFold"
 file=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $WORKDIR/input.lst)
 
 alphafold \
---run_relax=False \
+--models_to_relax=none \
 --data_dir=$ALPHAFOLD_DATA_DIR \
---uniref90_database_path=$ALPHAFOLD_DATA_DIR/uniref90/uniref90.fasta \
---mgnify_database_path=$ALPHAFOLD_DATA_DIR/mgnify/mgy_clusters.fa \
---bfd_database_path=$ALPHAFOLD_DATA_DIR/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
---uniref30_database_path=$ALPHAFOLD_DATA_DIR/uniref30/UniRef30_2021_03 \
---pdb_seqres_database_path=$ALPHAFOLD_DATA_DIR/pdb_seqres/pdb_seqres.txt \
---template_mmcif_dir=$ALPHAFOLD_DATA_DIR/pdb_mmcif/mmcif_files \
---obsolete_pdbs_path=$ALPHAFOLD_DATA_DIR/pdb_mmcif/obsolete.dat \
---uniprot_database_path=$ALPHAFOLD_DATA_DIR/uniprot/uniprot.fasta \
 --model_preset=multimer \
 --use_precomputed_msas=true \
 --num_multimer_predictions_per_model=1 \
