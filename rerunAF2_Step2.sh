261d43309c89e9866f8bcb99abaf8bda40c9184b
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-27 18:11:16 -0400

    try different gpu

--- rerunAF2_Step2.sh
+++ rerunAF2_Step2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=alphafold_step2
-#SBATCH --partition=batch
+#SBATCH --partition=gpu_p
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=32gb
+#SBATCH --gres=gpu:A100:1 #You can specify other GPU devices, like 'H100', here
+#SBATCH --mem=32gb                    
+#SBATCH --time=12:00:00           
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
-#SBATCH --gres=gpu:A100:1
-#SBATCH --time=6:00:00
 #SBATCH --output=AlphaFold2_Step2_StructurePred.%j.out
 #SBATCH --error=AlphaFold2_Step2_StructurePred.%j.err
 #SBATCH --array=8,16,17,24,25,26,30,31,37,39,48,49,50,54,55,59,67,70,75,82,94,96,112
