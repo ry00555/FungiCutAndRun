e55b79a74c5e07d183765ed780dc7b7dd242a410
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-27 18:06:31 -0400

    23 proteins failed

--- rerunAF2_Step2.sh
+++ rerunAF2_Step2.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=32gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
-#SBATCH --gres=gpu:V100:1
-#SBATCH --time=8:00:00
+#SBATCH --gres=gpu:A100:1
+#SBATCH --time=6:00:00
 #SBATCH --output=AlphaFold2_Step2_StructurePred.%j.out
 #SBATCH --error=AlphaFold2_Step2_StructurePred.%j.err
 #SBATCH --array=8,16,17,24,25,26,30,31,37,39,48,49,50,54,55,59,67,70,75,82,94,96,112
