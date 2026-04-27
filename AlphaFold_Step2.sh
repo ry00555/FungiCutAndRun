877d464f0a3b73849fe5acee4a23e28d4fd7c09e
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-27 18:14:57 -0400

    alphafolod step 2 fix

--- AlphaFold_Step2.sh
+++ AlphaFold_Step2.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=32gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ry00555@uga.edu
-#SBATCH --gres=gpu:V100:1
+#SBATCH --gres=gpu:V100:1 
 #SBATCH --time=4:00:00
 #SBATCH --output=AlphaFold2_Step2_StructurePred.%j.out
 #SBATCH --error=AlphaFold2_Step2_StructurePred.%j.err
