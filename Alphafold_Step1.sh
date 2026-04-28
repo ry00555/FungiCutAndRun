95fd4ea5ed9af59f2b7612fe656d0f708767ff90
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-04-28 12:53:44 -0400

    according to the cluster they changed it

--- Alphafold_Step1.sh
+++ Alphafold_Step1.sh
@@ -23,14 +23,6 @@ file=$(awk "NR==${SLURM_ARRAY_TASK_ID}" $WORKDIR/FastaforAlphaFold/input.lst)
 alphafold \
 --models_to_relax=none \
 --data_dir=$ALPHAFOLD_DATA_DIR \
---uniref90_database_path=$ALPHAFOLD_DATA_DIR/uniref90/uniref90.fasta \
---mgnify_database_path=$ALPHAFOLD_DATA_DIR/mgnify/mgy_clusters.fa \
---bfd_database_path=$ALPHAFOLD_DATA_DIR/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
---uniref30_database_path=$ALPHAFOLD_DATA_DIR/uniref30/UniRef30_2021_03 \
---pdb_seqres_database_path=$ALPHAFOLD_DATA_DIR/pdb_seqres/pdb_seqres.txt \
---template_mmcif_dir=$ALPHAFOLD_DATA_DIR/pdb_mmcif/mmcif_files \
---obsolete_pdbs_path=$ALPHAFOLD_DATA_DIR/pdb_mmcif/obsolete.dat \
---uniprot_database_path=$ALPHAFOLD_DATA_DIR/uniprot/uniprot.fasta \
 --model_preset=multimer \
 --num_multimer_predictions_per_model=1 \
 --max_template_date=2023-10-01 \
