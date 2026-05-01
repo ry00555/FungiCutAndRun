97a19a63dd254331bc01a92f9b95e0ccdf56c878
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-05-01 16:29:04 -0400

    try a chain script

--- Batches_AF3.sh
+++ Batches_AF3.sh
@@ -5,16 +5,25 @@ BATCH_SIZE=5
 MAX_RUNNING=1
 SCRIPT="EAF3_AF3_Pool.sh"
 
-for START in $(seq 1 $BATCH_SIZE $TOTAL); do
-    END=$((START + BATCH_SIZE - 1))
+START=${1:-1}
+END=$((START + BATCH_SIZE - 1))
 
-    if [ $END -gt $TOTAL ]; then
-        END=$TOTAL
-    fi
+if [ $END -gt $TOTAL ]; then
+    END=$TOTAL
+fi
 
-    echo "Submitting array ${START}-${END}%${MAX_RUNNING}"
+echo "Submitting EAF3 AF3 array ${START}-${END}%${MAX_RUNNING}"
 
-    sbatch --array=${START}-${END}%${MAX_RUNNING} $SCRIPT
+JOBID=$(sbatch --parsable --array=${START}-${END}%${MAX_RUNNING} $SCRIPT)
 
-    sleep 2
-done
+echo "Submitted job $JOBID"
+
+NEXT_START=$((END + 1))
+
+if [ $NEXT_START -le $TOTAL ]; then
+    echo "Submitting dependency for next batch starting at $NEXT_START"
+
+    sbatch --dependency=afterok:${JOBID} Batches_AF3.sh $NEXT_START
+else
+    echo "All batches submitted."
+fi
