38cfd61c510d4159faad874657323075d1bd3c06
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-05-08 14:56:04 -0400

    decrease jobs

--- Batches_Boltz2.sh
+++ Batches_Boltz2.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ASH1
 #SBATCH --partition=inter_p
 #SBATCH --ntasks=1
-#SBATCH --mem=12gb
+#SBATCH --mem=1gb
 #SBATCH --time=01:00:00
 #SBATCH --output=/scratch/ry00555/Boltz2/ASH1/logs/boltz2_batch.%j.out
 #SBATCH --error=/scratch/ry00555/Boltz2/ASH1/logs/boltz2_batch.%j.err
@@ -12,7 +12,7 @@
 #SBATCH --error=/scratch/ry00555/Boltz2/ASH1/logs/boltz2_%A_%a.err
 
 TOTAL=294       
-BATCH_SIZE=20    # how many jobs to submit at once (stay under QOS limit of 20)
+BATCH_SIZE==10    # how many jobs to submit at once (stay under QOS limit of 20)
 MAX_RUNNING=8    # how many run simultaneously (QOS limit is 8)
 SCRIPT="/home/ry00555/Research/FungiCutAndRun/Boltz2_Step1.sh"
 
@@ -38,7 +38,7 @@ NEXT_START=$((END + 1))
 
 if [ $NEXT_START -le $TOTAL ]; then
     echo "Submitting dependency for next batch starting at $NEXT_START"
-    sbatch --dependency=afterok:${JOBID} Batches_Boltz2_ASH1.sh $NEXT_START
+    sbatch --dependency=afterok:${JOBID} Boltz2_Step1.sh $NEXT_START
 else
     echo "All batches submitted."
 fi
