27a754c26960ec22911f19a5fd8c804c3e4fe641
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-06-23 20:34:25 -0400

    Update run_all_chromatin_domains.sh
    failed at finding cifs

--- run_all_chromatin_domains.sh
+++ run_all_chromatin_domains.sh
@@ -183,247 +183,247 @@ ALL_FASTA=${OUT}/metadata/all_species.fasta
 # PARSE PFAM DOMAINS
 ##################################################
 
-python3 <<'PY'
-
-import csv
-
-DOMAINS={
-
-"BAH":"BAH",
-"CBAH":"BAH",
-
-"PHD":"PHD",
-"PHD_2":"PHD",
-"PHD_3":"PHD",
-"PHD_4":"PHD",
-"PHD_5":"PHD",
-"PHD_like":"PHD",
-"PHD_JHD1":"PHD",
-"PHD_ash2p_like":"PHD",
-"zf-PHD-like":"PHD",
-
-"PWWP":"PWWP",
-"MUM1-like_PWWP":"PWWP",
-
-"Chromo":"Chromo",
-"Chromo_2":"Chromo",
-"Chromo_shadow":"ChromoShadow",
-
-"Bromodomain":"Bromodomain",
-
-"MRG":"MRG",
-"PHF12_MRG_bd":"MRG",
-
-"MBT":"MBT",
-
-"JmjC":"JmjC",
-"JmjC_2":"JmjC",
-"JmjN":"JmjN",
-
-"SET":"SET",
-"N-SET":"SET",
-"Pre-SET":"SET",
-"preSET_CXC":"SET",
-"SET_assoc":"SET",
-"SET7_N":"SET",
-
-"DOT1":"DOT1",
-"53-BP1_Tudor":"Tudor",
-"Crb2_Tudor":"Tudor",
-"SGF29_Tudor":"Tudor",
-"SMN_Tudor":"Tudor",
-"Tudor_2":"Tudor",
-"Tudor_3":"Tudor",
-"Tudor_4":"Tudor",
-"Tudor_5":"Tudor",
-"WD40":"WD40"
-}
-
-
-
-inp="chromatin_domain_results/metadata/chromatin_hits.domtbl"
-
-out="chromatin_domain_results/metadata/domain_occurrences.tsv"
-
-
-
-rows=[]
-
-
-for line in open(inp):
-
-    if line.startswith("#"):
-        continue
-
-
-    x=line.split()
-
-
-    if len(x)<23:
-        continue
-
-
-    pfam=x[0]
-
-
-    if pfam not in DOMAINS:
-        continue
-
-
-    protein=x[3]
-
-
-    parts=protein.split("|")
-
-
-    if len(parts) < 4:
-        continue
-
-    species=parts[0]
-    accession=parts[2]
-    gene=parts[3]
-
-    rows.append([
-
-        species,
-        accession,
-        gene,
-        DOMAINS[pfam],
-        x[17],
-        x[18],
-        x[6]
-
-    ])
-
-
-
-with open(out,"w") as f:
-
-    w=csv.writer(f,delimiter="\t")
-
-    w.writerow([
-    "species",
-    "accession",
-    "gene",
-    "domain",
-    "start",
-    "end",
-    "evalue"
-    ])
-
-    w.writerows(rows)
-
-
-
-print("PFAM domains:",len(rows))
-
-
-PY
-
-
-
-##################################################
-# EXTRACT DOMAIN SEQUENCES
-##################################################
-
-python3 <<'PY'
-
-
-from Bio import SeqIO
-import csv
-
-
-fasta="chromatin_domain_results/metadata/all_species.fasta"
-
-dom="chromatin_domain_results/metadata/domain_occurrences.tsv"
-
-
-records={}
-
-for r in SeqIO.parse(fasta,"fasta"):
-
-    records[r.id]=r
-
-
-out={}
-
-
-with open(dom) as f:
-
-    for row in csv.DictReader(f,delimiter="\t"):
-
-
-        key=f"{row['species']}|sp|{row['accession']}|{row['gene']}"
-
-
-        if key not in records:
-            continue
-
-
-        seq=records[key].seq
-
-
-        start=int(row["start"])
-        end=int(row["end"])
-
-
-        domain=row["domain"]
-
-
-        new=records[key][start-1:end]
-
-
-        new.id=f"{row['gene']}_{domain}"
-
-
-        out.setdefault(domain,[]).append(new)
-
-
-
-for domain,seqs in out.items():
-
-    SeqIO.write(
-    seqs,
-    f"chromatin_domain_results/domain_sequences/{domain}.fasta",
-    "fasta"
-    )
-
-
-
-PY
+# python3 <<'PY'
+#
+# import csv
+#
+# DOMAINS={
+#
+# "BAH":"BAH",
+# "CBAH":"BAH",
+#
+# "PHD":"PHD",
+# "PHD_2":"PHD",
+# "PHD_3":"PHD",
+# "PHD_4":"PHD",
+# "PHD_5":"PHD",
+# "PHD_like":"PHD",
+# "PHD_JHD1":"PHD",
+# "PHD_ash2p_like":"PHD",
+# "zf-PHD-like":"PHD",
+#
+# "PWWP":"PWWP",
+# "MUM1-like_PWWP":"PWWP",
+#
+# "Chromo":"Chromo",
+# "Chromo_2":"Chromo",
+# "Chromo_shadow":"ChromoShadow",
+#
+# "Bromodomain":"Bromodomain",
+#
+# "MRG":"MRG",
+# "PHF12_MRG_bd":"MRG",
+#
+# "MBT":"MBT",
+#
+# "JmjC":"JmjC",
+# "JmjC_2":"JmjC",
+# "JmjN":"JmjN",
+#
+# "SET":"SET",
+# "N-SET":"SET",
+# "Pre-SET":"SET",
+# "preSET_CXC":"SET",
+# "SET_assoc":"SET",
+# "SET7_N":"SET",
+#
+# "DOT1":"DOT1",
+# "53-BP1_Tudor":"Tudor",
+# "Crb2_Tudor":"Tudor",
+# "SGF29_Tudor":"Tudor",
+# "SMN_Tudor":"Tudor",
+# "Tudor_2":"Tudor",
+# "Tudor_3":"Tudor",
+# "Tudor_4":"Tudor",
+# "Tudor_5":"Tudor",
+# "WD40":"WD40"
+# }
+#
+#
+#
+# inp="chromatin_domain_results/metadata/chromatin_hits.domtbl"
+#
+# out="chromatin_domain_results/metadata/domain_occurrences.tsv"
+#
+#
+#
+# rows=[]
+#
+#
+# for line in open(inp):
+#
+#     if line.startswith("#"):
+#         continue
+#
+#
+#     x=line.split()
+#
+#
+#     if len(x)<23:
+#         continue
+#
+#
+#     pfam=x[0]
+#
+#
+#     if pfam not in DOMAINS:
+#         continue
+#
+#
+#     protein=x[3]
+#
+#
+#     parts=protein.split("|")
+#
+#
+#     if len(parts) < 4:
+#         continue
+#
+#     species=parts[0]
+#     accession=parts[2]
+#     gene=parts[3]
+#
+#     rows.append([
+#
+#         species,
+#         accession,
+#         gene,
+#         DOMAINS[pfam],
+#         x[17],
+#         x[18],
+#         x[6]
+#
+#     ])
+#
+#
+#
+# with open(out,"w") as f:
+#
+#     w=csv.writer(f,delimiter="\t")
+#
+#     w.writerow([
+#     "species",
+#     "accession",
+#     "gene",
+#     "domain",
+#     "start",
+#     "end",
+#     "evalue"
+#     ])
+#
+#     w.writerows(rows)
+#
+#
+#
+# print("PFAM domains:",len(rows))
+#
+#
+# PY
+#
+#
+#
+# ##################################################
+# # EXTRACT DOMAIN SEQUENCES
+# ##################################################
+#
+# python3 <<'PY'
+#
+#
+# from Bio import SeqIO
+# import csv
+#
+#
+# fasta="chromatin_domain_results/metadata/all_species.fasta"
+#
+# dom="chromatin_domain_results/metadata/domain_occurrences.tsv"
+#
+#
+# records={}
+#
+# for r in SeqIO.parse(fasta,"fasta"):
+#
+#     records[r.id]=r
+#
+#
+# out={}
+#
+#
+# with open(dom) as f:
+#
+#     for row in csv.DictReader(f,delimiter="\t"):
+#
+#
+#         key=f"{row['species']}|sp|{row['accession']}|{row['gene']}"
+#
+#
+#         if key not in records:
+#             continue
+#
+#
+#         seq=records[key].seq
+#
+#
+#         start=int(row["start"])
+#         end=int(row["end"])
+#
+#
+#         domain=row["domain"]
+#
+#
+#         new=records[key][start-1:end]
+#
+#
+#         new.id=f"{row['gene']}_{domain}"
+#
+#
+#         out.setdefault(domain,[]).append(new)
+#
+#
+#
+# for domain,seqs in out.items():
+#
+#     SeqIO.write(
+#     seqs,
+#     f"chromatin_domain_results/domain_sequences/{domain}.fasta",
+#     "fasta"
+#     )
+#
+#
+#
+# PY
 
 
 
 ##################################################
 # BUILD DOMAIN HMMs
 ##################################################
-
-for f in ${OUT}/domain_sequences/*.fasta
-
-do
-
-domain=$(basename $f .fasta)
-
-nseq=$(grep -c "^>" $f)
-
-if [ $nseq -lt 2 ]
-then
-    echo "Skipping ${domain} (only ${nseq} sequence)"
-    continue
-fi
-
-mafft \
---auto \
-$f \
-> ${OUT}/domain_sequences/${domain}.aln.fasta
-
-
-hmmbuild \
-${OUT}/domain_hmms/${domain}.hmm \
-${OUT}/domain_sequences/${domain}.aln.fasta
-
-
-done
+#
+# for f in ${OUT}/domain_sequences/*.fasta
+#
+# do
+#
+# domain=$(basename $f .fasta)
+#
+# nseq=$(grep -c "^>" $f)
+#
+# if [ $nseq -lt 2 ]
+# then
+#     echo "Skipping ${domain} (only ${nseq} sequence)"
+#     continue
+# fi
+#
+# mafft \
+# --auto \
+# $f \
+# > ${OUT}/domain_sequences/${domain}.aln.fasta
+#
+#
+# hmmbuild \
+# ${OUT}/domain_hmms/${domain}.hmm \
+# ${OUT}/domain_sequences/${domain}.aln.fasta
+#
+#
+# done
 
 
 
@@ -435,46 +435,46 @@ done
 FUNGAL_FASTA=${OUT}/metadata/fungal_proteomes.fasta
 
 
-> ${FUNGAL_FASTA}
-
-
-for f in \
-${BASE}/ncr_proteome.fasta \
-${BASE}/fgr_proteome.fasta \
-${BASE}/mgr_proteome.fasta \
-${BASE}/cne_proteome.fasta \
-${BASE}/zt_proteome.fasta
-
-
-do
-
-species=$(basename ${f} _proteome.fasta)
-
-sed "s/^>/>${species}|/" ${f} >> ${FUNGAL_FASTA}
-
-
-done
-
-
-
-for hmm in ${OUT}/domain_hmms/*.hmm
-
-do
-
-
-domain=$(basename ${hmm} .hmm)
-
-
-hmmsearch \
---cpu ${SLURM_CPUS_PER_TASK} \
---domtblout ${OUT}/metadata/${domain}_fungal_hits.domtbl \
--E 1e-3 \
-${hmm} \
-${FUNGAL_FASTA}
-
-
-
-done
+# > ${FUNGAL_FASTA}
+#
+#
+# for f in \
+# ${BASE}/ncr_proteome.fasta \
+# ${BASE}/fgr_proteome.fasta \
+# ${BASE}/mgr_proteome.fasta \
+# ${BASE}/cne_proteome.fasta \
+# ${BASE}/zt_proteome.fasta
+#
+#
+# do
+#
+# species=$(basename ${f} _proteome.fasta)
+#
+# sed "s/^>/>${species}|/" ${f} >> ${FUNGAL_FASTA}
+#
+#
+# done
+#
+#
+#
+# for hmm in ${OUT}/domain_hmms/*.hmm
+#
+# do
+#
+#
+# domain=$(basename ${hmm} .hmm)
+#
+#
+# hmmsearch \
+# --cpu ${SLURM_CPUS_PER_TASK} \
+# --domtblout ${OUT}/metadata/${domain}_fungal_hits.domtbl \
+# -E 1e-3 \
+# ${hmm} \
+# ${FUNGAL_FASTA}
+#
+#
+#
+# done
 
 
 
@@ -483,89 +483,89 @@ done
 # MERGE ALL DOMAIN HITS
 ##################################################
 
-python3 <<'PY'
-
-import csv
-from pathlib import Path
-
-
-base=Path(
-"chromatin_domain_results"
-)
-
-
-out=base/"metadata/domain_occurrences_extended.tsv"
-
-
-rows=[]
-
-
-with open(base/"metadata/domain_occurrences.tsv") as f:
-
-    rows=list(csv.reader(f,delimiter="\t"))
-
-
-
-for domtbl in Path(base/"metadata").glob("*_fungal_hits.domtbl"):
-
-
-    domain=domtbl.name.replace("_fungal_hits.domtbl","")
-
-
-    for line in open(domtbl):
-
-
-        if line.startswith("#"):
-            continue
-
-
-        x=line.split()
-
-
-        if len(x)<23:
-            continue
-
-
-        prot=x[0]
-
-        parts=prot.split("|")
-
-
-        rows.append([
-
-        parts[0],
-        parts[2],
-        parts[3],
-        domain,
-        x[17],
-        x[18],
-        x[6]
-
-        ])
-
-
-
-with open(out,"w") as f:
-
-    w=csv.writer(f,delimiter="\t")
-
-    w.writerow([
-    "species",
-    "accession",
-    "gene",
-    "domain",
-    "start",
-    "end",
-    "evalue"
-    ])
-
-    w.writerows(rows)
-
-
-
-print("Extended hits:",len(rows))
-
-PY
+# python3 <<'PY'
+#
+# import csv
+# from pathlib import Path
+#
+#
+# base=Path(
+# "chromatin_domain_results"
+# )
+#
+#
+# out=base/"metadata/domain_occurrences_extended.tsv"
+#
+#
+# rows=[]
+#
+#
+# with open(base/"metadata/domain_occurrences.tsv") as f:
+#
+#     rows=list(csv.reader(f,delimiter="\t"))
+#
+#
+#
+# for domtbl in Path(base/"metadata").glob("*_fungal_hits.domtbl"):
+#
+#
+#     domain=domtbl.name.replace("_fungal_hits.domtbl","")
+#
+#
+#     for line in open(domtbl):
+#
+#
+#         if line.startswith("#"):
+#             continue
+#
+#
+#         x=line.split()
+#
+#
+#         if len(x)<23:
+#             continue
+#
+#
+#         prot=x[0]
+#
+#         parts=prot.split("|")
+#
+#
+#         rows.append([
+#
+#         parts[0],
+#         parts[2],
+#         parts[3],
+#         domain,
+#         x[17],
+#         x[18],
+#         x[6]
+#
+#         ])
+#
+#
+#
+# with open(out,"w") as f:
+#
+#     w=csv.writer(f,delimiter="\t")
+#
+#     w.writerow([
+#     "species",
+#     "accession",
+#     "gene",
+#     "domain",
+#     "start",
+#     "end",
+#     "evalue"
+#     ])
+#
+#     w.writerows(rows)
+#
+#
+#
+# print("Extended hits:",len(rows))
+#
+# PY
 
 
 
@@ -574,48 +574,48 @@ PY
 # DOMAIN ARCHITECTURE
 ##################################################
 
-python3 <<'PY'
-
-
-from collections import defaultdict
-
-
-d=defaultdict(list)
-
-
-for line in open(
-"chromatin_domain_results/metadata/domain_occurrences_extended.tsv"
-):
-
-
-    if line.startswith("species"):
-        continue
-
-
-    s,a,g,dom,*_=line.strip().split("\t")
-
-
-    d[(s,g)].append(dom)
-
-
-
-with open(
-"chromatin_domain_results/metadata/domain_architecture.tsv",
-"w"
-) as out:
-
-
-    out.write("species\tgene\tarchitecture\n")
-
-
-    for k,v in d.items():
-
-        out.write(
-        f"{k[0]}\t{k[1]}\t{';'.join(sorted(set(v)))}\n"
-        )
-
-
-PY
+# python3 <<'PY'
+#
+#
+# from collections import defaultdict
+#
+#
+# d=defaultdict(list)
+#
+#
+# for line in open(
+# "chromatin_domain_results/metadata/domain_occurrences_extended.tsv"
+# ):
+#
+#
+#     if line.startswith("species"):
+#         continue
+#
+#
+#     s,a,g,dom,*_=line.strip().split("\t")
+#
+#
+#     d[(s,g)].append(dom)
+#
+#
+#
+# with open(
+# "chromatin_domain_results/metadata/domain_architecture.tsv",
+# "w"
+# ) as out:
+#
+#
+#     out.write("species\tgene\tarchitecture\n")
+#
+#
+#     for k,v in d.items():
+#
+#         out.write(
+#         f"{k[0]}\t{k[1]}\t{';'.join(sorted(set(v)))}\n"
+#         )
+#
+#
+# PY
 
 
 
@@ -625,7 +625,6 @@ PY
 ##################################################
 
 python3 <<'PY'
-
 
 import csv
 from pathlib import Path
@@ -637,28 +636,44 @@ ROOT=Path(
 )
 
 
-CIFS=list(ROOT.glob("cif_*"))
-
-print("Indexing CIF files...")
-
-cif_index={}
-
-for c in CIFS:
-    for f in c.rglob("*.cif"):
-        cif_index[f.name]=f
-
-print("Indexed",len(cif_index),"CIFs")
-
-out=ROOT/"chromatin_domain_results/domains_extracted"
+CIFS=[
+ROOT/"cif_human",
+ROOT/"cif_mouse",
+ROOT/"cif_zebrafish",
+ROOT/"cif_fungi"
+]
 
 
-out.mkdir(exist_ok=True)
+OUT=ROOT/"chromatin_domain_results/domains_extracted"
+
+OUT.mkdir(exist_ok=True)
+
+
+# load fasta headers to recover gene names
+fasta_headers={}
+
+with open(ROOT/"chromatin_domain_results/metadata/all_species.fasta") as f:
+
+    for line in f:
+
+        if line.startswith(">"):
+
+            ID=line.strip()[1:]
+
+            gene="NA"
+
+            match=re.search(r"GN=([^\s]+)",ID)
+
+            if match:
+                gene=match.group(1)
+
+            fasta_headers[ID]=gene
 
 
 
 for row in csv.DictReader(
-open(ROOT/"chromatin_domain_results/metadata/domain_occurrences_extended.tsv"),
-delimiter="\t"
+    open(ROOT/"chromatin_domain_results/metadata/domain_occurrences_extended.tsv"),
+    delimiter="\t"
 ):
 
 
@@ -668,13 +683,20 @@ delimiter="\t"
         continue
 
 
-        cif=None
+    cif=None
 
-        for name,path in cif_index.items():
 
-            if accession in name:
-                cif=path
-                break
+    # search all CIF folders recursively
+    for c in CIFS:
+
+        hits=list(c.rglob(f"*{accession}*.cif"))
+
+        if hits:
+
+            cif=hits[0]
+            break
+
+
 
     if cif is None:
         continue
@@ -705,12 +727,14 @@ delimiter="\t"
 
             res=int(x[8])
 
+
             if int(row["start"]) <= res <= int(row["end"]):
 
                 atoms.append(line)
 
 
         except:
+
             pass
 
 
@@ -720,16 +744,29 @@ delimiter="\t"
 
 
 
+    protein=row["gene"]
+
+
+    # recover actual gene name
+    gene=fasta_headers.get(
+        f"{row['species']}|sp|{accession}|{protein}",
+        protein
+    )
+
+
     domain=row["domain"]
 
 
-    d=out/domain
+    d=OUT/domain
 
     d.mkdir(exist_ok=True)
 
 
 
-    name=f"{row['gene']}_{domain}_{row['start']}-{row['end']}.pdb"
+    name=(
+        f"{gene}_{protein}_{domain}_"
+        f"{row['start']}-{row['end']}.pdb"
+    )
 
 
     with open(d/name,"w") as f:
@@ -739,8 +776,8 @@ delimiter="\t"
         f.write("END\n")
 
 
-print("done")
 
+print("Finished extracting domains")
 
 PY
 
