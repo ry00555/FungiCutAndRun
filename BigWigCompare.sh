fea4e79585a27901672d57eb213f18e18219b9e1
ry00555 <111528595+ry00555@users.noreply.github.com>
2026-07-22 02:08:36 -0400

    Update BigWigCompare.sh

--- BigWigCompare.sh
+++ BigWigCompare.sh
@@ -22,7 +22,7 @@ THREADS=12
 
 # ---- Paths ----
 BASEDIR="/scratch/ry00555/EpigeneticMemoryPaper2026/ChIPSeq/RemappedBW"
-META_CSV="${BASEDIR}/EAF3_MetaV3_H3K36me3_CSAW.csv"
+META_CSV="${BASEDIR}/EAF3_MetaV4_H3K36me3_CSAW.csv"
 AVG_DIR="${BASEDIR}/AveragedBW"
 NORM_DIR="${BASEDIR}/Log2InputNormBW"
 RATIO_DIR="${BASEDIR}/InputNormRatioBW"
